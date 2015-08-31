2ade72cf5a6786445f13fa3aab025ef24685ccd9
Ryan Shea <ryan@shea.io>
2015-08-31 19:47:57 -0400

    implement AuthRequest and AuthResponse objects and write a basic testing suite for them

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 var exports = {}
-exports.AuthRequest = require('./lib/auth-request')
-exports.AuthResponse = require('./lib/auth-response')
-exports.AuthMessage = require('./lib/auth-message')
+exports.AuthRequest = require('./lib/auth-request').AuthRequest
+exports.AuthResponse = require('./lib/auth-response').AuthResponse
+exports.AuthMessage = require('./lib/auth-message').AuthMessage
 module.exports = exports
