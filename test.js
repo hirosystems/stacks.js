39561c6b44615430cc9b54a9e7329633cfdaea7e
Ryan Shea <ryan@shea.io>
2015-09-02 16:22:24 -0400

    update layout of test.js

--- test.js
+++ test.js
@@ -21,13 +21,11 @@ test('testAuthRequest', function(t) {
         sampleEncodedToken = 'eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3N1ZWRBdCI6IjE0NDA3MTM0MTQuMTkiLCJjaGFsbGVuZ2UiOiIxZDc4NTBkNy01YmNmLTQ3ZDAtYTgxYy1jMDA4NTc5NzY1NDQiLCJwZXJtaXNzaW9ucyI6WyJibG9ja2NoYWluaWQiXSwiaXNzdWVyIjp7InB1YmxpY0tleSI6IjAzODI3YjZhMzRjZWJlZTZkYjEwZDEzNzg3ODQ2ZGVlYWMxMDIzYWNiODNhN2I4NjZlMTkyZmEzNmI5MTkwNjNlNCIsImRvbWFpbiI6Im9uZW5hbWUuY29tIn19.96Q_O_4DX8uPy1enosEwS2sIcyVelWhxvfj2F8rOvHldhqt9YRYilauepb95DVnmpqpCXxJb7jurT8auNCbptw',
         sampleDecodedTokenPayload = {"issuedAt": "1440713414.19", "challenge": "1d7850d7-5bcf-47d0-a81c-c00857976544", "permissions": ["blockchainid"], "issuer": {"publicKey": "03827b6a34cebee6db10d13787846deeac1023acb83a7b866e192fa36b919063e4", "domain": "onename.com"}}
 
-    var authRequest = new AuthRequest(privateKeyHex, publicKeyHex, issuingDomain, permissions)
+    var authRequest = new AuthRequest(privateKeyHex, publicKeyHex, issuingDomain, permissions),
+        authRequestToken = authRequest.token(),
+        decodedAuthRequestToken = authRequest.decode()
     t.ok(authRequest)
-
-    var authRequestToken = authRequest.token()
     t.ok(authRequestToken)
-
-    var decodedAuthRequestToken = authRequest.decode()
     t.ok(decodedAuthRequestToken)
 
     var decodedSampleToken = AuthRequest.decode(sampleEncodedToken)
@@ -52,17 +50,16 @@ test('testAuthResponse', function(t) {
         sampleEncodedToken = 'eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3N1ZWRBdCI6IjE0NDA3MTM0MTQuODUiLCJjaGFsbGVuZ2UiOiI3Y2Q5ZWQ1ZS1iYjBlLTQ5ZWEtYTMyMy1mMjhiZGUzYTA1NDkiLCJpc3N1ZXIiOnsicHVibGljS2V5IjoiMDNmZGQ1N2FkZWMzZDQzOGVhMjM3ZmU0NmIzM2VlMWUwMTZlZGE2YjU4NWMzZTI3ZWE2NjY4NmMyZWE1MzU4NDc5IiwiY2hhaW5QYXRoIjoiYmQ2Mjg4NWVjM2YwZTM4MzgwNDMxMTVmNGNlMjVlZWRkMjJjYzg2NzExODAzZmIwYzE5NjAxZWVlZjE4NWUzOSIsInB1YmxpY0tleWNoYWluIjoieHB1YjY2MU15TXdBcVJiY0ZRVnJRcjRRNGtQamFQNEpqV2FmMzlmQlZLalBkSzZvR0JheUU0NkdBbUt6bzVVRFBRZExTTTlEdWZaaVA4ZWF1eTU2WE51SGljQnlTdlpwN0o1d3N5UVZwaTJheHpaIiwiYmxvY2tjaGFpbmlkIjoicnlhbiJ9fQ.oO7ROPKq3T3X0azAXzHsf6ub6CYy5nUUFDoy8MS22B3TlYisqsBrRtzWIQcSYiFXLytrXwAdt6vjehj3OFioDQ',
         sampleDecodedTokenPayload = {"issuedAt": "1440713414.85", "challenge": "7cd9ed5e-bb0e-49ea-a323-f28bde3a0549", "issuer": {"publicKey": "03fdd57adec3d438ea237fe46b33ee1e016eda6b585c3e27ea66686c2ea5358479", "chainPath": "bd62885ec3f0e3838043115f4ce25eedd22cc86711803fb0c19601eeef185e39", "publicKeychain": "xpub661MyMwAqRbcFQVrQr4Q4kPjaP4JjWaf39fBVKjPdK6oGBayE46GAmKzo5UDPQdLSM9DufZiP8eauy56XNuHicBySvZp7J5wsyQVpi2axzZ", "blockchainid": "ryan"}}
 
-    var authResponse = new AuthResponse(privateKeyHex, publicKeyHex, challenge, blockchainid, publicKeychain, chainPath)
+    var authResponse = new AuthResponse(privateKeyHex, publicKeyHex, challenge, blockchainid, publicKeychain, chainPath),
+        authResponseToken = authResponse.token(),
+        decodedAuthRequestToken = authResponse.decode()
     t.ok(authResponse)
-
-    var authResponseToken = authResponse.token()
     t.ok(authResponseToken)
-
-    var decodedAuthRequestToken = authResponse.decode()
     t.ok(decodedAuthRequestToken)
     t.equal(decodedAuthRequestToken.payload.issuer.publicKey, publicKeyHex)
 
     var decodedSampleToken = AuthResponse.decode(sampleEncodedToken)
+
     t.ok(decodedSampleToken)
     t.equal(JSON.stringify(decodedSampleToken.payload), JSON.stringify(sampleDecodedTokenPayload))
 
