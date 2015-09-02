9c3cc6d4a3bbedee86845634a359a108c9bbc772
Ryan Shea <ryan@shea.io>
2015-09-02 13:52:13 -0400

    update identity verification, fix keychain child derivation, ensure public keys are decompressed before conversion to PEMs

--- test.js
+++ test.js
@@ -12,10 +12,10 @@ var resolver = new OnenameClient(process.env.ONENAME_APP_ID, process.env.ONENAME
 test('testAuthRequest', function(t) {
     t.plan(6)
 
-    var privateKeyHex = 'fa2e921324f5c7c9603b421bfedc106cdf5f6bbe8e1e77266f51bb4c7dd403cd',
-        publicKeyHex = '044303d1f2b5a9be69836b6e091af6a38b2cfa060c6f2aabdb7db1939e100d61a533338fff15739c1778f329ea0c0c052c0cec06847bf9dc61fda4cc0961c2ff19',
-        privateKeyHex2 = 'a5c61c6ca7b3e7e55edee68566aeab22e4da26baa285c7bd10e8d2218aa3b22901',
-        publicKeyHex2 = '027d28f9951ce46538951e3697c62588a87f1f1f295de4a14fdd4c780fc52cfe69',
+    var privateKeyHex = 'a5c61c6ca7b3e7e55edee68566aeab22e4da26baa285c7bd10e8d2218aa3b229',
+        publicKeyHex = '027d28f9951ce46538951e3697c62588a87f1f1f295de4a14fdd4c780fc52cfe69',
+        //privateKeyHex = 'fa2e921324f5c7c9603b421bfedc106cdf5f6bbe8e1e77266f51bb4c7dd403cd',
+        //publicKeyHex = '044303d1f2b5a9be69836b6e091af6a38b2cfa060c6f2aabdb7db1939e100d61a533338fff15739c1778f329ea0c0c052c0cec06847bf9dc61fda4cc0961c2ff19',
         issuingDomain = 'onename.com',
         permissions = ['blockchainid'],
         sampleEncodedToken = 'eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3N1ZWRBdCI6IjE0NDA3MTM0MTQuMTkiLCJjaGFsbGVuZ2UiOiIxZDc4NTBkNy01YmNmLTQ3ZDAtYTgxYy1jMDA4NTc5NzY1NDQiLCJwZXJtaXNzaW9ucyI6WyJibG9ja2NoYWluaWQiXSwiaXNzdWVyIjp7InB1YmxpY0tleSI6IjAzODI3YjZhMzRjZWJlZTZkYjEwZDEzNzg3ODQ2ZGVlYWMxMDIzYWNiODNhN2I4NjZlMTkyZmEzNmI5MTkwNjNlNCIsImRvbWFpbiI6Im9uZW5hbWUuY29tIn19.96Q_O_4DX8uPy1enosEwS2sIcyVelWhxvfj2F8rOvHldhqt9YRYilauepb95DVnmpqpCXxJb7jurT8auNCbptw',
@@ -41,11 +41,9 @@ test('testAuthRequest', function(t) {
 
 test('testAuthResponse', function(t) {
     t.plan(7)
-    
-    var privateKeyHex = 'fa2e921324f5c7c9603b421bfedc106cdf5f6bbe8e1e77266f51bb4c7dd403cd',
-        publicKeyHex = '044303d1f2b5a9be69836b6e091af6a38b2cfa060c6f2aabdb7db1939e100d61a533338fff15739c1778f329ea0c0c052c0cec06847bf9dc61fda4cc0961c2ff19',
-        privateKeyHex2 = '278a5de700e29faae8e40e366ec5012b5ec63d36ec77e8a2417154cc1d25383f',
-        publicKeyHex2 = '03fdd57adec3d438ea237fe46b33ee1e016eda6b585c3e27ea66686c2ea5358479',
+
+    var privateKeyHex = '278a5de700e29faae8e40e366ec5012b5ec63d36ec77e8a2417154cc1d25383f',
+        publicKeyHex = '03fdd57adec3d438ea237fe46b33ee1e016eda6b585c3e27ea66686c2ea5358479',
         publicKeychain = 'xpub661MyMwAqRbcFQVrQr4Q4kPjaP4JjWaf39fBVKjPdK6oGBayE46GAmKzo5UDPQdLSM9DufZiP8eauy56XNuHicBySvZp7J5wsyQVpi2axzZ',
         privateKeychain = 'xprv9s21ZrQH143K2vRPJpXPhcT12MDpL3rofvjagwKn4yZpPPFpgWn1cy1Wwp3pk78wfHSLcdyZhmEBQsZ29ZwFyTQhhkVVa9QgdTC7hGMB1br',
         chainPath = 'bd62885ec3f0e3838043115f4ce25eedd22cc86711803fb0c19601eeef185e39',
