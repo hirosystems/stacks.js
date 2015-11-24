42846ceabbbf494be1429a8338e5714c49d23dc6
Ryan Shea <ryan@shea.io>
2015-11-23 19:17:53 -0500

    improve interface for setting permissions and issuers

--- README.md
+++ README.md
@@ -30,8 +30,7 @@ var AuthRequest = require('blockchain-auth').AuthRequest,
 ```js
 > var privateKey = 'a5c61c6ca7b3e7e55edee68566aeab22e4da26baa285c7bd10e8d2218aa3b229'
 > var authRequest = new AuthRequest(privateKey)
-> var appBlockchainID = 'onename'
-> authRequest.prepare(appBlockchainId, ['blockchainid'])
+> authRequest.setIssuer({ blockchainid: 'onename.id' })
 > requestToken = authRequest.sign()
 'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpc3N1ZXIiOnsicHVibGljS2V5IjoiMDI3ZDI4Zjk5NTFjZTQ2NTM4OTUxZTM2OTdjNjI1ODhhODdmMWYxZjI5NWRlNGExNGZkZDRjNzgwZmM1MmNmZTY5IiwiYmxvY2tjaGFpbmlkIjoib25lbmFtZSJ9LCJpc3N1ZWRBdCI6MTQ0NDI1ODkzNTI1MSwiY2hhbGxlbmdlIjoiMGI0MjcyMmItZTc4MS00MzRhLTgwNWQtYzA5YzQ3NmU4NmI5IiwicGVybWlzc2lvbnMiOlsiYmxvY2tjaGFpbmlkIl19.4sMvmUQ6q5DuAEXYaVIwVSe1nzd4KjfU3hwfoUztEAx9Gwr5XmS9-sBQZ2iS_x4uxzG2de1CHlw-14ZrB9ejww'
 ```
@@ -41,13 +40,14 @@ var AuthRequest = require('blockchain-auth').AuthRequest,
 ```js
 > decodedRequestToken = decodeToken(requestToken)
 { header: { typ: 'JWT', alg: 'ES256' },
-  payload: 
-   { issuer: 
-      { publicKey: '027d28f9951ce46538951e3697c62588a87f1f1f295de4a14fdd4c780fc52cfe69',
-        blockchainid: 'onename' },
-     issuedAt: 1444258935251,
-     challenge: '0b42722b-e781-434a-805d-c09c476e86b9',
-     permissions: [ 'blockchainid' ] },
+  payload: {
+    issuer: { publicKey: '027d28f9951ce46538951e3697c62588a87f1f1f295de4a14fdd4c780fc52cfe69',blockchainid: 'onename' },
+    issuedAt: 1444258935251,
+    permissions: [
+      { action: 'sign', data: '0b42722b-e781-434a-805d-c09c476e86b9' },
+      { action: 'disclose', scope: 'username' }
+    ]
+  },
   signature: '4sMvmUQ6q5DuAEXYaVIwVSe1nzd4KjfU3hwfoUztEAx9Gwr5XmS9-sBQZ2iS_x4uxzG2de1CHlw-14ZrB9ejww' }
 ```
 
@@ -68,7 +68,7 @@ verifyAuthMessage(authRequestToken, blockchainIdResolver, function(verified) {
 ```js
 > var privateKey = '278a5de700e29faae8e40e366ec5012b5ec63d36ec77e8a2417154cc1d25383f'
 > var authResponse = new AuthResponse(privateKey)
-> var userBlockchainId = 'ryan'
+> var userBlockchainId = 'ryan.id'
 > var publicKeychain = 'xpub661MyMwAqRbcFQVrQr4Q4kPjaP4JjWaf39fBVKjPdK6oGBayE46GAmKzo5UDPQdLSM9DufZiP8eauy56XNuHicBySvZp7J5wsyQVpi2axzZ'
 > var chainPath = 'bd62885ec3f0e3838043115f4ce25eedd22cc86711803fb0c19601eeef185e39'
 > authResponse.prepare(decodedRequestToken.payload.challenge, userBlockchainId, publicKeychain, chainPath)
@@ -84,7 +84,7 @@ verifyAuthMessage(authRequestToken, blockchainIdResolver, function(verified) {
   payload: 
    { issuer: 
       { publicKey: '03fdd57adec3d438ea237fe46b33ee1e016eda6b585c3e27ea66686c2ea5358479',
-        blockchainid: 'ryan',
+        blockchainid: 'ryan.id',
         publicKeychain: 'xpub661MyMwAqRbcFQVrQr4Q4kPjaP4JjWaf39fBVKjPdK6oGBayE46GAmKzo5UDPQdLSM9DufZiP8eauy56XNuHicBySvZp7J5wsyQVpi2axzZ',
         chainPath: 'bd62885ec3f0e3838043115f4ce25eedd22cc86711803fb0c19601eeef185e39' },
      issuedAt: 1444259422196,
--- test.js
+++ test.js
@@ -11,9 +11,9 @@ var test = require('tape'),
 var onenameResolver = new OnenameClient(process.env.ONENAME_APP_ID, process.env.ONENAME_APP_SECRET)
 
 function testBlockchainIDResolver(blockchainids, resolve, reject) {
-    if (blockchainids[0] === 'onename') {
+    if (blockchainids[0] === 'onename.id') {
         resolve({
-            "onename": {
+            "onename.id": {
                 "profile": {
                     "auth": [
                         {
@@ -23,9 +23,9 @@ function testBlockchainIDResolver(blockchainids, resolve, reject) {
                 }
             }
         })
-    } else if (blockchainids[0] === 'ryan') {
+    } else if (blockchainids[0] === 'ryan.id') {
         resolve({
-            "ryan": {
+            "ryan.id": {
                 "profile": {
                     "auth": [
                         {
@@ -44,21 +44,54 @@ function testAuthRequest() {
     var privateKey = 'a5c61c6ca7b3e7e55edee68566aeab22e4da26baa285c7bd10e8d2218aa3b229',
         publicKey = '027d28f9951ce46538951e3697c62588a87f1f1f295de4a14fdd4c780fc52cfe69'
 
-    var issuingBlockchainID = 'onename',
-        permissions = ['blockchainid'],
-        sampleToken = 'eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3N1ZWRBdCI6IjE0NDA3MTM0MTQuMTkiLCJjaGFsbGVuZ2UiOiIxZDc4NTBkNy01YmNmLTQ3ZDAtYTgxYy1jMDA4NTc5NzY1NDQiLCJwZXJtaXNzaW9ucyI6WyJibG9ja2NoYWluaWQiXSwiaXNzdWVyIjp7InB1YmxpY0tleSI6IjAzODI3YjZhMzRjZWJlZTZkYjEwZDEzNzg3ODQ2ZGVlYWMxMDIzYWNiODNhN2I4NjZlMTkyZmEzNmI5MTkwNjNlNCIsImRvbWFpbiI6Im9uZW5hbWUuY29tIn19.96Q_O_4DX8uPy1enosEwS2sIcyVelWhxvfj2F8rOvHldhqt9YRYilauepb95DVnmpqpCXxJb7jurT8auNCbptw',
+    var sampleToken = 'eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3N1ZWRBdCI6IjE0NDA3MTM0MTQuMTkiLCJjaGFsbGVuZ2UiOiIxZDc4NTBkNy01YmNmLTQ3ZDAtYTgxYy1jMDA4NTc5NzY1NDQiLCJwZXJtaXNzaW9ucyI6WyJibG9ja2NoYWluaWQiXSwiaXNzdWVyIjp7InB1YmxpY0tleSI6IjAzODI3YjZhMzRjZWJlZTZkYjEwZDEzNzg3ODQ2ZGVlYWMxMDIzYWNiODNhN2I4NjZlMTkyZmEzNmI5MTkwNjNlNCIsImRvbWFpbiI6Im9uZW5hbWUuY29tIn19.96Q_O_4DX8uPy1enosEwS2sIcyVelWhxvfj2F8rOvHldhqt9YRYilauepb95DVnmpqpCXxJb7jurT8auNCbptw',
         sampleTokenPayload = {"issuedAt": "1440713414.19", "challenge": "1d7850d7-5bcf-47d0-a81c-c00857976544", "permissions": ["blockchainid"], "issuer": {"publicKey": "03827b6a34cebee6db10d13787846deeac1023acb83a7b866e192fa36b919063e4", "domain": "onename.com"}}
 
     test('basicRequest', function(t) {
         t.plan(4)
 
+        var issuingBlockchainID = 'onename.id'
+
         var authRequest = new AuthRequest(privateKey)
 
-        authRequest.prepare(issuingBlockchainID, permissions)
+        authRequest.setIssuer({
+            blockchainid: issuingBlockchainID
+        })
         
         var authRequestToken = authRequest.sign(),
             decodedAuthRequestToken = decodeToken(authRequestToken)
         
+        t.ok(authRequest instanceof AuthRequest, 'authRequest should be a valid AuthMessage object')
+        t.equal(typeof authRequestToken, 'string', 'token should be a string')
+        t.equal(decodedAuthRequestToken.payload.issuer.blockchainid, issuingBlockchainID, 'token blockchain id should match the reference')
+
+        verifyAuthMessage(authRequestToken, testBlockchainIDResolver, function(verified) {
+            t.equal(verified, true, 'token should be verified')
+        }, function(err) {
+            console.log(err)
+        })
+    })
+
+    test('advancedRequest', function(t) {
+        t.plan(4)
+
+        var issuingBlockchainID = 'onename.id'
+
+        var authRequest = new AuthRequest(privateKey)
+
+        authRequest.setIssuer({
+            blockchainid: issuingBlockchainID,
+            appName: 'Onename',
+            appDomain: 'onename.com'
+        })
+        authRequest.setPermissions([
+            { action: 'disclose', scope: 'username' },
+            { action: 'write', data: { uuid: '34e57db64ce7435ab0f759oca31386527c670bd1' } }
+        ])
+
+        var authRequestToken = authRequest.sign(),
+            decodedAuthRequestToken = decodeToken(authRequestToken)
+
         t.ok(authRequest instanceof AuthRequest, 'authRequest should be a valid AuthMessage object')
         t.equal(typeof authRequestToken, 'string', 'token should be a string')
         t.equal(decodedAuthRequestToken.payload.issuer.blockchainid, issuingBlockchainID, 'token blockchain id should match the reference')
@@ -86,11 +119,11 @@ function testAuthResponse() {
         privateKeychain = 'xprv9s21ZrQH143K2vRPJpXPhcT12MDpL3rofvjagwKn4yZpPPFpgWn1cy1Wwp3pk78wfHSLcdyZhmEBQsZ29ZwFyTQhhkVVa9QgdTC7hGMB1br',
         chainPath = 'bd62885ec3f0e3838043115f4ce25eedd22cc86711803fb0c19601eeef185e39',
         challenge = '7cd9ed5e-bb0e-49ea-a323-f28bde3a0549',
-        blockchainid = 'ryan',
+        blockchainid = 'ryan.id',
         sampleToken = 'eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3N1ZWRBdCI6IjE0NDA3MTM0MTQuODUiLCJjaGFsbGVuZ2UiOiI3Y2Q5ZWQ1ZS1iYjBlLTQ5ZWEtYTMyMy1mMjhiZGUzYTA1NDkiLCJpc3N1ZXIiOnsicHVibGljS2V5IjoiMDNmZGQ1N2FkZWMzZDQzOGVhMjM3ZmU0NmIzM2VlMWUwMTZlZGE2YjU4NWMzZTI3ZWE2NjY4NmMyZWE1MzU4NDc5IiwiY2hhaW5QYXRoIjoiYmQ2Mjg4NWVjM2YwZTM4MzgwNDMxMTVmNGNlMjVlZWRkMjJjYzg2NzExODAzZmIwYzE5NjAxZWVlZjE4NWUzOSIsInB1YmxpY0tleWNoYWluIjoieHB1YjY2MU15TXdBcVJiY0ZRVnJRcjRRNGtQamFQNEpqV2FmMzlmQlZLalBkSzZvR0JheUU0NkdBbUt6bzVVRFBRZExTTTlEdWZaaVA4ZWF1eTU2WE51SGljQnlTdlpwN0o1d3N5UVZwaTJheHpaIiwiYmxvY2tjaGFpbmlkIjoicnlhbiJ9fQ.oO7ROPKq3T3X0azAXzHsf6ub6CYy5nUUFDoy8MS22B3TlYisqsBrRtzWIQcSYiFXLytrXwAdt6vjehj3OFioDQ',
         sampleTokenPayload = {"issuedAt": "1440713414.85", "challenge": "7cd9ed5e-bb0e-49ea-a323-f28bde3a0549", "issuer": {"publicKey": "03fdd57adec3d438ea237fe46b33ee1e016eda6b585c3e27ea66686c2ea5358479", "chainPath": "bd62885ec3f0e3838043115f4ce25eedd22cc86711803fb0c19601eeef185e39", "publicKeychain": "xpub661MyMwAqRbcFQVrQr4Q4kPjaP4JjWaf39fBVKjPdK6oGBayE46GAmKzo5UDPQdLSM9DufZiP8eauy56XNuHicBySvZp7J5wsyQVpi2axzZ", "blockchainid": "ryan"}},
         partiallyIdentifiedToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpc3N1ZXIiOnsicHVibGljS2V5IjoiMDNmZGQ1N2FkZWMzZDQzOGVhMjM3ZmU0NmIzM2VlMWUwMTZlZGE2YjU4NWMzZTI3ZWE2NjY4NmMyZWE1MzU4NDc5IiwiYmxvY2tjaGFpbmlkIjoicnlhbiIsInB1YmxpY0tleWNoYWluIjoieHB1YjY2MU15TXdBcVJiY0ZRVnJRcjRRNGtQamFQNEpqV2FmMzlmQlZLalBkSzZvR0JheUU0NkdBbUt6bzVVRFBRZExTTTlEdWZaaVA4ZWF1eTU2WE51SGljQnlTdlpwN0o1d3N5UVZwaTJheHpaIn0sImlzc3VlZEF0IjoxNDQxNzU1NjE5MDk2LCJjaGFsbGVuZ2UiOiI3Y2Q5ZWQ1ZS1iYjBlLTQ5ZWEtYTMyMy1mMjhiZGUzYTA1NDkiLCJpYXQiOjE0NDE3NTU2MTl9.1LxW_yg2z40Qd84x0kep0-7TWiDdTEoJbdYFUJ3qt297zxbwo8OOvYW43W6TMT5cloxur5wifq0WoOTdXw4C_Q',
-        incorrectBlockchainID = 'ryanshea'
+        incorrectBlockchainID = 'ryanshea.id'
 
     test('basicResponse', function(t) {
         t.plan(4)
