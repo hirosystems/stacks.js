1b722fe10db8bb262291382e856fe47ca0571942
Ryan Shea <ryan@shea.io>
2015-11-24 17:27:13 -0500

    add a provision system with separately satisfiable requests for signing and disclosure

--- test.js
+++ test.js
@@ -84,7 +84,7 @@ function testAuthRequest() {
             appName: 'Onename',
             appDomain: 'onename.com'
         })
-        authRequest.setPermissions([
+        authRequest.setProvisions([
             { action: 'disclose', scope: 'username' },
             { action: 'write', data: { uuid: '34e57db64ce7435ab0f759oca31386527c670bd1' } }
         ])
@@ -93,7 +93,7 @@ function testAuthRequest() {
             decodedAuthRequestToken = decodeToken(authRequestToken)
 
         console.log(authRequestToken)
-        console.log(decodedAuthRequestToken)
+        console.log(JSON.stringify(decodedAuthRequestToken, null, 2))
 
         t.ok(authRequest instanceof AuthRequest, 'authRequest should be a valid AuthMessage object')
         t.equal(typeof authRequestToken, 'string', 'token should be a string')
@@ -121,27 +121,35 @@ function testAuthResponse() {
         publicKeychain = 'xpub661MyMwAqRbcFQVrQr4Q4kPjaP4JjWaf39fBVKjPdK6oGBayE46GAmKzo5UDPQdLSM9DufZiP8eauy56XNuHicBySvZp7J5wsyQVpi2axzZ',
         privateKeychain = 'xprv9s21ZrQH143K2vRPJpXPhcT12MDpL3rofvjagwKn4yZpPPFpgWn1cy1Wwp3pk78wfHSLcdyZhmEBQsZ29ZwFyTQhhkVVa9QgdTC7hGMB1br',
         chainPath = 'bd62885ec3f0e3838043115f4ce25eedd22cc86711803fb0c19601eeef185e39',
-        challenge = '7cd9ed5e-bb0e-49ea-a323-f28bde3a0549',
+        provisions = [
+            { action: 'sign', data: '7cd9ed5e-bb0e-49ea-a323-f28bde3a0549' },
+            { action: 'disclose', scope: 'username' }
+        ],
+        challengeSignature = "3045022100963da1185591472e1bc867319ddd372593e17758724aaae49a2a30284b399bf0022037022ba279cf20421b96e9cac2a16ab6e415878cda488bc429fbc325f04315af",
         username = 'ryan.id',
         sampleToken = 'eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3N1ZWRBdCI6IjE0NDA3MTM0MTQuODUiLCJjaGFsbGVuZ2UiOiI3Y2Q5ZWQ1ZS1iYjBlLTQ5ZWEtYTMyMy1mMjhiZGUzYTA1NDkiLCJpc3N1ZXIiOnsicHVibGljS2V5IjoiMDNmZGQ1N2FkZWMzZDQzOGVhMjM3ZmU0NmIzM2VlMWUwMTZlZGE2YjU4NWMzZTI3ZWE2NjY4NmMyZWE1MzU4NDc5IiwiY2hhaW5QYXRoIjoiYmQ2Mjg4NWVjM2YwZTM4MzgwNDMxMTVmNGNlMjVlZWRkMjJjYzg2NzExODAzZmIwYzE5NjAxZWVlZjE4NWUzOSIsInB1YmxpY0tleWNoYWluIjoieHB1YjY2MU15TXdBcVJiY0ZRVnJRcjRRNGtQamFQNEpqV2FmMzlmQlZLalBkSzZvR0JheUU0NkdBbUt6bzVVRFBRZExTTTlEdWZaaVA4ZWF1eTU2WE51SGljQnlTdlpwN0o1d3N5UVZwaTJheHpaIiwiYmxvY2tjaGFpbmlkIjoicnlhbiJ9fQ.oO7ROPKq3T3X0azAXzHsf6ub6CYy5nUUFDoy8MS22B3TlYisqsBrRtzWIQcSYiFXLytrXwAdt6vjehj3OFioDQ',
         sampleTokenPayload = {"issuedAt": "1440713414.85", "challenge": "7cd9ed5e-bb0e-49ea-a323-f28bde3a0549", "issuer": {"publicKey": "03fdd57adec3d438ea237fe46b33ee1e016eda6b585c3e27ea66686c2ea5358479", "chainPath": "bd62885ec3f0e3838043115f4ce25eedd22cc86711803fb0c19601eeef185e39", "publicKeychain": "xpub661MyMwAqRbcFQVrQr4Q4kPjaP4JjWaf39fBVKjPdK6oGBayE46GAmKzo5UDPQdLSM9DufZiP8eauy56XNuHicBySvZp7J5wsyQVpi2axzZ", "blockchainid": "ryan"}},
         partiallyIdentifiedToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpc3N1ZXIiOnsicHVibGljS2V5IjoiMDNmZGQ1N2FkZWMzZDQzOGVhMjM3ZmU0NmIzM2VlMWUwMTZlZGE2YjU4NWMzZTI3ZWE2NjY4NmMyZWE1MzU4NDc5IiwiYmxvY2tjaGFpbmlkIjoicnlhbiIsInB1YmxpY0tleWNoYWluIjoieHB1YjY2MU15TXdBcVJiY0ZRVnJRcjRRNGtQamFQNEpqV2FmMzlmQlZLalBkSzZvR0JheUU0NkdBbUt6bzVVRFBRZExTTTlEdWZaaVA4ZWF1eTU2WE51SGljQnlTdlpwN0o1d3N5UVZwaTJheHpaIn0sImlzc3VlZEF0IjoxNDQxNzU1NjE5MDk2LCJjaGFsbGVuZ2UiOiI3Y2Q5ZWQ1ZS1iYjBlLTQ5ZWEtYTMyMy1mMjhiZGUzYTA1NDkiLCJpYXQiOjE0NDE3NTU2MTl9.1LxW_yg2z40Qd84x0kep0-7TWiDdTEoJbdYFUJ3qt297zxbwo8OOvYW43W6TMT5cloxur5wifq0WoOTdXw4C_Q',
-        invalidUsername = 'ryanshea.id'
+        invalidUsername = 'ryanshea.id',
+        privateData = {}
 
     test('basicResponse', function(t) {
-        t.plan(4)
+        t.plan(5)
 
         var authResponse = new AuthResponse(privateKeyHex)
 
-        authResponse.setChallenge(challenge)
+        authResponse.satisfyProvisions(provisions, username, privateData)
         authResponse.setIssuer(username, publicKeychain, chainPath)
 
         var authResponseToken = authResponse.sign(),
             decodedAuthResponseToken = decodeToken(authResponseToken)
 
+        console.log(JSON.stringify(decodedAuthResponseToken, null, 2))
+
         t.ok(authResponse instanceof AuthResponse, 'authRequest should be a valid AuthResponse object')
         t.equal(typeof authResponseToken, 'string', 'token should be a string')
         t.equal(decodedAuthResponseToken.payload.issuer.publicKey, publicKeyHex, 'token public key hex should match the reference value')
+        t.equal(decodedAuthResponseToken.payload.provisions[0].signature, challengeSignature, 'challenge signature should match the reference value')
 
         verifyAuthMessage(authResponseToken, testBlockchainIDResolver, function(verified) {
             t.equal(verified, true, 'token should be verified')
@@ -165,7 +173,7 @@ function testAuthResponse() {
 
         var authResponse = new AuthResponse(privateKeyHex, publicKeyHex)
 
-        authResponse.setChallenge(challenge)
+        authResponse.satisfyProvisions(provisions, invalidUsername, privateData)
         authResponse.setIssuer(invalidUsername, publicKeychain, chainPath)
 
         var authResponseToken = authResponse.sign()
@@ -183,7 +191,7 @@ function testAuthResponse() {
 
         var authResponse = new AuthResponse(privateKeyHex)
 
-        authResponse.setChallenge(challenge)
+        authResponse.satisfyProvisions(provisions)
 
         var authResponseToken = authResponse.sign(),
             decodedAuthResponseToken = decodeToken(authResponseToken)
