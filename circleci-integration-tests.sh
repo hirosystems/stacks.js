45bab781ee656fbc7664e7282a8c32986072833d
Jude Nelson <judecn@gmail.com>
2017-03-30 15:57:52 -0400

    use the right branch

--- circleci-integration-tests.sh
+++ circleci-integration-tests.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+BLOCKSTACK_BRANCH="rc-0.14.2"
+
 # needed on CircleCI's VMs
 pip install --upgrade pip
 pip install --upgrade six
@@ -8,8 +10,11 @@ pip install --upgrade cryptography
 
 # install Blockstack and integration tests
 git clone https://github.com/blockstack/blockstack-core /tmp/blockstack-core
+cd /tmp/blockstack-core && git checkout "$BLOCKSTACK_BRANCH"
+
 cd /tmp/blockstack-core && ./setup.py build && ./setup.py install 
 cd /tmp/blockstack-core/integration_tests && ./setup.py build && ./setup.py install
+
 npm install -g babel
 npm install -g browserify
 
