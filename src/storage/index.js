435713358d48f1c67f69cbc8f878b71fda903741
Larry Salibra <larry@appartisan.com>
2017-07-03 16:40:02 +0800

    document storage methods we expose #223

--- src/storage/index.js
+++ src/storage/index.js
@@ -1,3 +1,20 @@
 export {
-  getFile, putFile
+  /**
+   * Retrives the specified file from the app's data store.
+   * @param {String} path - the path to the file to read
+   * @returns {Promise} that resolves to the raw data in the file
+   * or rejects with an error
+   */
+  getFile
+} from 'blockstack-storage'
+
+export {
+  /**
+   * Stores the data provided in the file specified.
+   * @param {String} path - the path to store the data in
+   * @param {String|Buffer} contents - the data to store in the file
+   * @return {Promise} that resolves if the operation succeed and rejects
+   * if it failed
+   */
+  putFile
 } from 'blockstack-storage'
