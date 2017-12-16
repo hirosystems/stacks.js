eb51cb97b790dddd9aec01ba9806c993247ab69a
Valentin Sundermann <me@vsund.de>
2017-12-16 03:10:18 +0100

    Resolved the behaviour of a lacking lib/ folder on failed ts declaration

--- tests/typescriptTests/index.ts
+++ tests/typescriptTests/index.ts
@@ -1,13 +1,4 @@
-/*
- * This should theoretically import from '../../lib/index'
- * because deployed versions ship the lib/ directory, but since
- * the test logic leaves the lib/ directory deleted on a fail it's
- * more convenient for debugging to use the include from src/.
- * 
- * As long as the test logic doesn't get changed fundamentally,
- * this should work. This might bite developers while debugging!
- */
-import * as blockstack from '../../src/index';
+import * as blockstack from '../../lib/index';
 
 
 // TODO: Add tests for every typed method
