d5752f48959a08efa440467d267be27e4dabc6da
Valentin Sundermann <me@vsund.de>
2017-12-16 13:30:18 +0100

    Add declarations for newly exported storage methods

--- src/storage/index.d.ts
+++ src/storage/index.d.ts
@@ -1,3 +1,15 @@
 // from ./index
 export declare function getFile(path: string, decrypt?: boolean): Promise<any>;
 export declare function putFile(path: string, content: string|Buffer, encrypt?: boolean): Promise<any>;
+export declare function deleteFile(path: string): Promise<any>; // throws Error
+
+
+export declare type GaiaHubConfig = {
+  address: string,
+  url_prefix: string,
+  token: string,
+  server: string
+}
+export declare function connectToGaiaHub(gaiaHubUrl: string, challengeSignerHex: string): Promise<any>;
+export declare function uploadToGaiaHub(filename: string, contents: any, hubConfig: GaiaHubConfig, contentType?: string): Promise<any>;
+export declare const BLOCKSTACK_GAIA_HUB_LABEL = 'blockstack-gaia-hub-config';
