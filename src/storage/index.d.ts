eca321488bac20dd6acc261820931c6868f08309
Valentin Sundermann <me@vsund.de>
2018-01-13 02:15:35 +0100

    Add ts declarations for newly exported storage methods

--- src/storage/index.d.ts
+++ src/storage/index.d.ts
@@ -1,7 +1,19 @@
 // from ./index
-export declare function getFile(path: string, decrypt?: boolean): Promise<any>;
-export declare function putFile(path: string, content: string|Buffer, encrypt?: boolean): Promise<any>;
+export declare type GetFileOptions = {
+  decrypt?: boolean,
+  username?: string,
+  app?: string,
+  zoneFileLookupURL?: string
+}
+export declare type PutFileOptions = {
+  encrypt?: boolean
+}
+
+export declare function getUserAppFileUrl(path: string, username: string, appOrigin: string, zoneFileLookupURL: string): Promise<string>;
+export declare function getFile(path: string, options?: GetFileOptions): Promise<any>;
+export declare function putFile(path: string, content: string|Buffer, options?: PutFileOptions): Promise<any>;
 export declare function deleteFile(path: string): Promise<any>; // throws Error
+export declare function getAppBucketUrl(gaiaHubUrl: string, appPrivateKey: string): Promise<string>;
 
 
 export declare type GaiaHubConfig = {
@@ -12,4 +24,5 @@ export declare type GaiaHubConfig = {
 }
 export declare function connectToGaiaHub(gaiaHubUrl: string, challengeSignerHex: string): Promise<any>;
 export declare function uploadToGaiaHub(filename: string, contents: any, hubConfig: GaiaHubConfig, contentType?: string): Promise<any>;
+export declare function getBucketUrl(gaiaHubUrl: string, appPrivateKey: string): Promise<string>;
 export declare const BLOCKSTACK_GAIA_HUB_LABEL = 'blockstack-gaia-hub-config';
