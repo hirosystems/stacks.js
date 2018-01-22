4f4dd6f7d18f6572709a6db7cf0f1c021cd5903d
Aaron Blankstein <aaron@blockstack.com>
2018-01-22 17:43:56 -0500

    more test coverage

--- tests/unitTests/src/unitTestsOperations.js
+++ tests/unitTests/src/unitTestsOperations.js
@@ -2,7 +2,8 @@ import test from 'tape'
 import FetchMock from 'fetch-mock'
 import btc from 'bitcoinjs-lib'
 
-import { addUTXOsToFund, sumOutputValues } from '../../../lib/operations/utils'
+import { estimateTXBytes, addUTXOsToFund, sumOutputValues,
+         hash160, hash128, decodeB40 } from '../../../lib/operations/utils'
 
 import { transactions } from '../../../lib/'
 
@@ -13,8 +14,64 @@ const testAddresses = [
     address: '16xVjkJ3nY62B9t9q3N9wY6hx1duAfwRZR' }
 ]
 
-function runAddUTXOsToFundTests() {
-  test('addUTXOsToFundSingleUTXO', (t) => {
+function utilsTests() {
+  test('estimateTXBytes', (t) => {
+    t.plan(2)
+    let txHex = '010000000288e68977fab8038af07746e5d687652a44aa15f532509c202749d' +
+        'bad8a418733000000006b483045022100813ef3534b5030b544e5a5bd1db93f85dc89e2' +
+        'a565197a14784edff5564bd65b022008005213c6aa4c7ebe06cfd86bdaf3e662ae58371' +
+        '896a0a841e81106fbe1507401210236b07942707a86ab666bb300b58d295d988ce9c3a3' +
+        '38a0e08380dd98732fd4faffffffff3ba3edfd7a7b12b27ac72c3e67768f617fc81bc38' +
+        '88a51323a9fb8aa4b1e5e4a000000006b483045022100d0c9b1594137186a1dc6c0b3a6' +
+        'cbe08399b57e2b8c953584f2ce20bef5642eb902206b9c88b8d2d311db26601acf3068d' +
+        'd118649ead4a1f93d029a52c0c61cb2cd2901210236b07942707a86ab666bb300b58d29' +
+        '5d988ce9c3a338a0e08380dd98732fd4faffffffff030000000000000000296a2769643' +
+        'f363da95bc8d5203d1c07bd87c564a1e6395826cfdfe87cfd31ffa2a3b8101e3e93096f' +
+        '2b7c150000000000001976a91441577ec99314a293acbc17d8152137cf4862f7f188ace' +
+        '8030000000000001976a9142ebe7b4729185f68c7185c3c6af60fad1b6eeebf88ac00000000'
+    let tx = btc.Transaction.fromHex(txHex)
+    tx.ins.forEach(x => x.script = null)
+
+    let actualLength = txHex.length / 2
+    let estimatedLength = estimateTXBytes(tx, 0, 0)
+
+    let tx2 = new btc.TransactionBuilder()
+    tx2.addOutput(tx.outs[0].script, 0)
+    let estimatedLength2 = estimateTXBytes(tx2, 2, 2)
+
+    t.ok(estimatedLength >= actualLength - 5 && estimatedLength <= actualLength + 5,
+         `TX size estimate is roughly accurate? (estimated: ${estimatedLength}, actual: ${actualLength})`)
+    t.ok(estimatedLength2 >= actualLength - 5 && estimatedLength2 <= actualLength + 5,
+         `TX size estimate is roughly accurate? (estimated: ${estimatedLength2}, actual: ${actualLength})`)
+  })
+
+  test('encoding routines', (t) => {
+    t.plan(5)
+
+    t.equal(hash160(Buffer.from(
+      '99999566ahjhqwuywqehpzlzlzlzl09189128921jkjlqjosq')).toString('hex'),
+            '7ea1fa0f2003c31b015a72af9f4a5f104b5c2840')
+
+    t.equal(hash160(Buffer.from('1234')).toString('hex'),
+            'fd7a0d80999bedd76c9a0828057817fc6049a507')
+
+    t.equal(hash128(Buffer.from('999')).toString('hex'),
+            '83cf8b609de60036a8277bd0e9613575')
+
+    t.equal(hash128(Buffer.from('99999566ahjhqwuywqehpzlzlzlzl09189128921jkjlqjosqaaa')).toString('hex'),
+            '740ae7f18c939cf5e7c189a2c77a012f')
+
+    t.equal(decodeB40('0123456789abcdefghijklmnopqrstuvwxyz-_.+0123456789abcdefghi' +
+                      'jklmnopqrstuvwxyz-_.+0123456789abcdefghijklmnopqrstuvwxyz-_' +
+                      '.+0123456789abcdefghijklmnopqrstuvwxyz-_.+0123456789abcdefg' +
+                      'hijklmnopqrstuvwxyz-_.+'),
+            '384a516059e707615a1992d3101f6f346df3326d03ea7b673e3754078895db48da2d0' +
+            'fcb1bd89d618b0863bd8bac6db43a2d9cff5cc307310922d3cb8cf9c159d31c6a9c91' +
+            '03197263a4e88f52d1b77dfc610e1b8dc9616ba6c2d0a1b792f0d73784c698c69f34a' +
+            'e5e7900753627a3ac87529035fb1a6cba7ce2e1df590941cf30a44557')
+  })
+
+  test('not enough UTXOs to fund', (t) => {
     t.plan(1)
 
     let txB = new btc.TransactionBuilder()
@@ -117,6 +174,6 @@ function transactionTests() {
 }
 
 export function runOperationsTests() {
-  runAddUTXOsToFundTests()
+  utilsTests()
   transactionTests()
 }
