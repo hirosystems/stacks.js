2ab5bad08fbfcf44fc9161a62fa1f116b013e35d
Larry Salibra <larry@appartisan.com>
2018-02-18 19:41:31 +0200

    document authentication verification methods #370
    Signed-off-by: Larry Salibra <larry@appartisan.com>

--- src/auth/authVerification.js
+++ src/auth/authVerification.js
@@ -3,6 +3,15 @@ import { decodeToken, TokenVerifier } from 'jsontokens'
 import { getAddressFromDID, publicKeyToAddress,
   isSameOriginAbsoluteUrl, fetchAppManifest } from '../index'
 
+/**
+ * Checks if the ES256k signature on passed `token` match the claimed public key
+ * in the payload key `public_keys`.
+ *
+ * @param  {String} token encoded and signed authentication token
+ * @return {Boolean} Returns `true` if the signature matches the claimed public key
+ * @throws {Error} if `token` contains multiple public keys
+ * @private
+ */
 export function doSignaturesMatchPublicKeys(token: string) {
   const payload = decodeToken(token).payload
   const publicKeys = payload.public_keys
@@ -24,6 +33,16 @@ export function doSignaturesMatchPublicKeys(token: string) {
   }
 }
 
+/**
+ * Makes sure that the identity address portion of
+ * the decentralized identifier passed in the issuer `iss`
+ * key of the token matches the public key
+ *
+ * @param  {String} token encoded and signed authentication token
+ * @return {Boolean} if the identity address and public keys match
+ * @throws {Error} if ` token` has multiple public keys
+ * @private
+ */
 export function doPublicKeysMatchIssuer(token: string) {
   const payload = decodeToken(token).payload
   const publicKeys = payload.public_keys
@@ -41,6 +60,19 @@ export function doPublicKeysMatchIssuer(token: string) {
   return false
 }
 
+/**
+ * Looks up the identity address that owns the claimed username
+ * in `token` using the lookup endpoint provided in `nameLookupURL`
+ * to determine if the username is owned by the identity address
+ * that matches the claimed public key
+ *
+ * @param  {String} token  encoded and signed authentication token
+ * @param  {String} nameLookupURL a URL to the name lookup endpoint of the Blockstack Core API
+ * @return {Promise<Boolean>} returns a `Promise` that resolves to
+ * `true` if the username is owned by the public key, otherwise the
+ * `Promise` resolves to `false`
+ * @private
+ */
 export function doPublicKeysMatchUsername(token: string,
   nameLookupURL: string) {
   return new Promise((resolve) => {
@@ -90,6 +122,15 @@ export function doPublicKeysMatchUsername(token: string,
   })
 }
 
+/**
+ * Checks if the if the token issuance time and date is after the
+ * current time and date.
+ *
+ * @param  {String}  token encoded and signed authentication token
+ * @return {Boolean} `true` if the token was issued after the current time,
+ * otherwise returns `false`
+ * @private
+ */
 export function isIssuanceDateValid(token: string) {
   const payload = decodeToken(token).payload
   if (payload.iat) {
@@ -107,6 +148,12 @@ export function isIssuanceDateValid(token: string) {
   }
 }
 
+/**
+ * Checks if the expiration date of the `token` is before the current time
+ * @param  {String}  token encoded and signed authentication token
+ * @return {Boolean} `true` if the `token` has not yet expired, `false`
+ * if the `token` has expired
+ */
 export function isExpirationDateValid(token: string) {
   const payload = decodeToken(token).payload
   if (payload.exp) {
@@ -124,21 +171,39 @@ export function isExpirationDateValid(token: string) {
   }
 }
 
+/**
+ * Makes sure the `manifest_uri` is a same origin absolute URL.
+ * @param  {String}  token encoded and signed authentication token
+ * @return {Boolean} `true` if valid, otherwise `false`
+ * @private
+ */
 export function isManifestUriValid(token: string) {
   const payload = decodeToken(token).payload
   return isSameOriginAbsoluteUrl(payload.domain_name, payload.manifest_uri)
 }
 
+/**
+ * Makes sure the `redirect_uri` is a same origin absolute URL.
+ * @param  {String}  token encoded and signed authentication token
+ * @return {Boolean} `true` if valid, otherwise `false`
+ */
 export function isRedirectUriValid(token: string) {
   const payload = decodeToken(token).payload
   return isSameOriginAbsoluteUrl(payload.domain_name, payload.redirect_uri)
 }
 
 /**
- * Verify authentication request is valid
- * @param  {String} token [description]
+ * Verify authentication request is valid. This function performs a number
+ * of checks on the authentication request token:
+ * * Checks that `token` has a valid issuance date & is not expired
+ * * Checks that `token` has a valid signature that matches the public key it claims
+ * * Checks that both the manifest and redirect URLs are absolute and conform to
+ * the same origin policy
+ *
+ * @param  {String} token encoded and signed authentication request token
  * @return {Promise} that resolves to true if the auth request
- *  is valid and false if it does not
+ *  is valid and false if it does not. It rejects with a String if the
+ *  token is not signed
  *  @private
  */
 export function verifyAuthRequest(token: string) {
@@ -165,9 +230,9 @@ export function verifyAuthRequest(token: string) {
 }
 
 /**
- * Verify the authentication response is valid and
+ * Verify the authentication request is valid and
  * fetch the app manifest file if valid. Otherwise, reject the promise.
- * @param  {String} token the authentication request token
+ * @param  {String} token encoded and signed authentication request token
  * @return {Promise} that resolves to the app manifest file in JSON format
  * or rejects if the auth request or app manifest file is invalid
  * @private
