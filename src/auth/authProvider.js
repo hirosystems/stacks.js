132396a16c13545e8150be48aa59a1bfd925a88e
Larry Salibra <larry@appartisan.com>
2018-02-19 23:11:22 +0100

    document auth provider methods #376

--- src/auth/authProvider.js
+++ src/auth/authProvider.js
@@ -3,6 +3,12 @@ import { decodeToken } from 'jsontokens'
 import { updateQueryStringParameter } from '../index'
 import { BLOCKSTACK_HANDLER } from '../utils'
 
+/**
+ * Retrieves the authentication request from the query string
+ * @return {String|null} the authentication request or `null` if
+ * the query string parameter `authRequest` is not found
+ * @private
+ */
 export function getAuthRequestFromURL() {
   const queryDict = queryString.parse(location.search)
   if (queryDict.authRequest !== null && queryDict.authRequest !== undefined) {
@@ -12,6 +18,15 @@ export function getAuthRequestFromURL() {
   }
 }
 
+/**
+ * Fetches the contents of the manifest file specified in the authentication request
+ *
+ * @param  {String} authRequest encoded and signed authentication request
+ * @return {Promise<Object|String>} Returns a `Promise` that resolves to the JSON
+ * object manifest file unless there's an error in which case rejects with an error
+ * message.
+ * @private
+ */
 export function fetchAppManifest(authRequest) {
   return new Promise((resolve, reject) => {
     if (!authRequest) {
@@ -38,6 +53,17 @@ export function fetchAppManifest(authRequest) {
   })
 }
 
+/**
+ * Redirect the user's browser to the app using the `redirect_uri`
+ * specified in the authentication request, passing the authentication
+ * response token as a query parameter.
+ *
+ * @param {String} authRequest  encoded and signed authentication request token
+ * @param  {String} authResponse encoded and signed authentication response token
+ * @return {undefined}
+ * @throws {Error} if there is no redirect uri
+ * @private
+ */
 export function redirectUserToApp(authRequest, authResponse) {
   const payload = decodeToken(authRequest).payload
   let redirectURI = payload.redirect_uri
