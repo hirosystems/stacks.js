c79bcedde20402b0165fb98e0ac852dafad1b0a7
Jude Nelson <judecn@gmail.com>
2018-07-26 18:22:56 -0400

    when connecting to a gaia hub, taken a gaia association token as an optional argument and try to load the one from local user data if present.  This makes it possible to use private gaia hubs with blockstack.js

--- src/storage/hub.js
+++ src/storage/hub.js
@@ -65,7 +65,10 @@ function makeLegacyAuthToken(challengeText: string, signerKeyHex: string): strin
   }
 }
 
-function makeV1GaiaAuthToken(hubInfo: Object, signerKeyHex: string, hubUrl: string): string {
+function makeV1GaiaAuthToken(hubInfo: Object,
+                             signerKeyHex: string,
+                             hubUrl: string, 
+                             associationToken?: string): string {
   const challengeText = hubInfo.challenge_text
   const handlesV1Auth = (hubInfo.latest_auth_version
                          && parseInt(hubInfo.latest_auth_version.slice(1), 10) >= 1)
@@ -80,21 +83,36 @@ function makeV1GaiaAuthToken(hubInfo: Object, signerKeyHex: string, hubUrl: stri
     gaiaChallenge: challengeText,
     hubUrl,
     iss,
-    salt 
+    salt,
+    associationToken
   }
+  
   const token = new TokenSigner('ES256K', signerKeyHex).sign(payload)
   return `v1:${token}`
 }
 
 export function connectToGaiaHub(gaiaHubUrl: string,
-                                 challengeSignerHex: string): Promise<GaiaHubConfig> {
+                                 challengeSignerHex: string,
+                                 associationToken?: string): Promise<GaiaHubConfig> {
+  if (!associationToken) {
+    // maybe given in local storage?
+    try {
+      const userData = loadUserData()
+      if (userData && userData.gaiaAssociationToken) {
+        associationToken = userData.gaiaAssociationToken
+      }
+    } catch(e) {
+      ;
+    }
+  }
+
   Logger.debug(`connectToGaiaHub: ${gaiaHubUrl}/hub_info`)
 
   return fetch(`${gaiaHubUrl}/hub_info`)
     .then(response => response.json())
     .then((hubInfo) => {
       const readURL = hubInfo.read_url_prefix
-      const token = makeV1GaiaAuthToken(hubInfo, challengeSignerHex, gaiaHubUrl)
+      const token = makeV1GaiaAuthToken(hubInfo, challengeSignerHex, gaiaHubUrl, associationToken)
       const address = hexStringToECPair(challengeSignerHex
                                         + (challengeSignerHex.length === 64 ? '01' : ''))
         .getAddress()
@@ -127,7 +145,7 @@ export function setLocalGaiaHubConnection(): Promise<GaiaHubConfig> {
     userData = loadUserData()
   }
 
-  return connectToGaiaHub(userData.hubUrl, userData.appPrivateKey)
+  return connectToGaiaHub(userData.hubUrl, userData.appPrivateKey, userData.associationToken)
     .then((gaiaConfig) => {
       localStorage.setItem(BLOCKSTACK_GAIA_HUB_LABEL, JSON.stringify(gaiaConfig))
       return gaiaConfig
