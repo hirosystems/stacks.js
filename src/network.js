693de9f0f780fe4e0d0e8dc738fa23d03579c9a4
Jude Nelson <judecn@gmail.com>
2018-09-26 19:19:07 -0400

    add price v1/v2 API compatibility code and account querying code

--- src/network.js
+++ src/network.js
@@ -2,6 +2,7 @@
 import bitcoinjs from 'bitcoinjs-lib'
 import FormData from 'form-data'
 import bigi from 'bigi'
+import RIPEMD160 from 'ripemd160'
 import { MissingParameterError, RemoteServiceError } from './errors'
 import { Logger } from './logger'
 
@@ -14,6 +15,12 @@ const SATOSHIS_PER_BTC = 1e8
 const TX_BROADCAST_SERVICE_ZONE_FILE_ENDPOINT = 'zone-file'
 const TX_BROADCAST_SERVICE_REGISTRATION_ENDPOINT = 'registration'
 const TX_BROADCAST_SERVICE_TX_ENDPOINT = 'transaction'
+
+export function hash160(buff: Buffer) {
+  const sha256 = bitcoinjs.crypto.sha256(buff)
+  return (new RIPEMD160()).update(sha256).digest()
+}
+
 
 export class BitcoinNetwork {
   broadcastTransaction(transaction: string) : Promise<Object> {
@@ -70,11 +77,80 @@ export class BlockstackNetwork {
     return this.coerceAddress('1111111111111111111114oLvT2')
   }
 
+  /**
+   * Get the price of a name via the legacy /v1/prices API endpoint.
+   * @param {String} fullyQualifiedName the name to query
+   * @return {Promise} a promise to an Object with { units: String, amount: BigInteger }
+   * @private
+   */
+  getNamePriceV1(fullyQualifiedName: string) : Promise<*> {
+    // legacy code path
+    return fetch(`${this.blockstackAPIUrl}/v1/prices/names/${fullyQualifiedName}`)
+      .then((resp) => {
+        if (!resp.ok) {
+          throw new Error(`Failed to query name price for ${fullyQualifiedName}`)
+        }
+        return resp
+      })
+      .then(resp => resp.json())
+      .then(resp => resp.name_price)
+      .then((namePrice) => {
+        if (!namePrice || !namePrice.satoshis) {
+          throw new Error(
+            `Failed to get price for ${fullyQualifiedName}. Does the namespace exist?`)
+        }
+        if (namePrice.satoshis < this.DUST_MINIMUM) {
+          namePrice.satoshis = this.DUST_MINIMUM
+        }
+        const result = {
+          units: 'BTC',
+          amount: bigi.fromByteArrayUnsigned(String(namePrice.satoshis))
+        }
+        return result
+      })
+  }
 
-  getNamePrice(fullyQualifiedName: string) : Promise<*> {
+  /**
+   * Get the price of a namespace via the legacy /v1/prices API endpoint.
+   * @param {String} namespaceID the namespace to query
+   * @return {Promise} a promise to an Object with { units: String, amount: BigInteger }
+   * @private
+   */
+  getNamespacePriceV1(namespaceID: string) : Promise<*> {
+    // legacy code path
+    return fetch(`${this.blockstackAPIUrl}/v1/prices/namespaces/${namespaceID}`)
+      .then((resp) => {
+        if (!resp.ok) {
+          throw new Error(`Failed to query name price for ${namespaceID}`)
+        }
+        return resp
+      })
+      .then(resp => resp.json())
+      .then((namespacePrice) => {
+        if (!namespacePrice || !namespacePrice.satoshis) {
+          throw new Error(`Failed to get price for ${namespaceID}`)
+        }
+        if (namespacePrice.satoshis < this.DUST_MINIMUM) {
+          namespacePrice.satoshis = this.DUST_MINIMUM
+        }
+        const result = {
+          units: 'BTC',
+          amount: bigi.fromByteArrayUnsigned(String(namespacePrice.satoshis))
+        }
+        return result
+      })
+  }
+  
+  /**
+   * Get the price of a name via the /v2/prices API endpoint.
+   * @param {String} fullyQualifiedName the name to query
+   * @return {Promise} a promise to an Object with { units: String, amount: BigInteger }
+   * @private
+   */
+  getNamePriceV2(fullyQualifiedName: string) : Promise<*> {
     return fetch(`${this.blockstackAPIUrl}/v2/prices/names/${fullyQualifiedName}`)
       .then((resp) => {
-        if (resp.status === 404) {
+        if (resp.status !== 200) {
           // old core node 
           throw new Error('The upstream node does not handle the /v2/ price namespace')
         }
@@ -87,21 +163,31 @@ export class BlockstackNetwork {
           throw new Error(
             `Failed to get price for ${fullyQualifiedName}. Does the namespace exist?`)
         }
-        if (namePrice.units === 'BTC' && namePrice.amount < this.DUST_MINIMUM) {
-          namePrice.amount = this.DUST_MINIMUM
-        }
         const result = {
           units: namePrice.units,
           amount: bigi.fromByteArrayUnsigned(namePrice.amount)
+        }
+        if (namePrice.units === 'BTC') {
+          // must be at least dust-minimum
+          const dustMin = bigi.fromByteArrayUnsigned(String(this.DUST_MINIMUM))
+          if (result.amount.compareTo(dustMin) < 0) {
+            result.amount = dustMin
+          }
         }
         return result
       })
   }
 
-  getNamespacePrice(namespaceID: string) : Promise<*> {
+  /**
+   * Get the price of a namespace via the /v2/prices API endpoint.
+   * @param {String} namespaceID the namespace to query
+   * @return {Promise} a promise to an Object with { units: String, amount: BigInteger }
+   * @private
+   */
+  getNamespacePriceV2(namespaceID: string) : Promise<*> {
     return fetch(`${this.blockstackAPIUrl}/v2/prices/namespaces/${namespaceID}`)
       .then((resp) => {
-        if (resp.status === 404) {
+        if (resp.status !== 200) {
           // old core node 
           throw new Error('The upstream node does not handle the /v2/ price namespace')
         }
@@ -112,21 +198,65 @@ export class BlockstackNetwork {
         if (!namespacePrice) {
           throw new Error(`Failed to get price for ${namespaceID}`)
         }
-        if (namespacePrice.units === 'BTC' && namespacePrice.amount < this.DUST_MINIMUM) {
-          namespacePrice.amount = this.DUST_MINIMUM
-        }
         const result = {
           units: namespacePrice.units,
           amount: bigi.fromByteArrayUnsigned(namespacePrice.amount)
+        }
+        if (namespacePrice.units === 'BTC') {
+          // must be at least dust-minimum
+          const dustMin = bigi.fromByteArrayUnsigned(String(this.DUST_MINIMUM))
+          if (result.amount.compareTo(dustMin) < 0) {
+            result.amount = dustMin
+          }
         }
         return result
       })
   }
 
+  /**
+   * Get the price of a name.
+   * @param {String} fullyQualifiedName the name to query
+   * @return {Promise} a promise to an Object with { units: String, amount: BigInteger }, where
+   *   .units encodes the cryptocurrency units to pay (e.g. BTC, STACKS), and
+   *   .amount encodes the number of units, in the smallest denominiated amount
+   *   (e.g. if .units is BTC, .amount will be satoshis; if .units is STACKS, 
+   *   .amount will be microStacks)
+   */
+  getNamePrice(fullyQualifiedName: string) : Promise<*> {
+    // handle v1 or v2 
+    return Promise.resolve().then(() => this.getNamePriceV2(fullyQualifiedName))
+      .catch(() => this.getNamePriceV1(fullyQualifiedName))
+  }
+
+  /**
+   * Get the price of a namespace
+   * @param {String} namespaceID the namespace to query
+   * @return {Promise} a promise to an Object with { units: String, amount: BigInteger }, where
+   *   .units encodes the cryptocurrency units to pay (e.g. BTC, STACKS), and
+   *   .amount encodes the number of units, in the smallest denominiated amount
+   *   (e.g. if .units is BTC, .amount will be satoshis; if .units is STACKS, 
+   *   .amount will be microStacks)
+   */
+  getNamespacePrice(namespaceID: string) : Promise<*> {
+    // handle v1 or v2 
+    return Promise.resolve().then(() => this.getNamespacePriceV2(namespaceID))
+      .catch(() => this.getNamespacePriceV1(namespaceID))
+  }
+
+  /**
+   * How many blocks can pass between a name expiring and the name being able to be
+   * re-registered by a different owner?
+   * @return {Promise} a promise to the number of blocks
+   */
   getGracePeriod() {
     return new Promise(resolve => resolve(5000))
   }
 
+  /**
+   * Get the names -- both on-chain and off-chain -- owned by an address.
+   * @param {String} address the blockchain address (the hash of the owner public key)
+   * @return {Promise} a promise that resolves to a list of names (Strings)
+   */
   getNamesOwned(address: string) {
     const networkAddress = this.coerceAddress(address)
     return fetch(`${this.blockstackAPIUrl}/v1/addresses/bitcoin/${networkAddress}`)
@@ -134,6 +264,12 @@ export class BlockstackNetwork {
       .then(obj => obj.names)
   }
 
+  /**
+   * Get the blockchain address to which a name's registration fee must be sent
+   * (the address will depend on the namespace in which it is registered.)
+   * @param {String} namespace the namespace ID
+   * @return {Promise} a promise that resolves to an address (String)
+   */
   getNamespaceBurnAddress(namespace: string) {
     return Promise.all([
       fetch(`${this.blockstackAPIUrl}/v1/namespaces/${namespace}`),
@@ -147,9 +283,9 @@ export class BlockstackNetwork {
         }
       })
       .then(([namespaceInfo, blockHeight]) => {
-        let address = '1111111111111111111114oLvT2' // default burn address
+        let address = this.getDefaultBurnAddress()
         if (namespaceInfo.version === 2) {
-        // pay-to-namespace-creator if this namespace is less than 1 year old
+          // pay-to-namespace-creator if this namespace is less than 1 year old
           if (namespaceInfo.reveal_block + 52595 >= blockHeight) {
             address = namespaceInfo.address
           }
@@ -159,6 +295,12 @@ export class BlockstackNetwork {
       .then(address => this.coerceAddress(address))
   }
 
+  /**
+   * Get WHOIS-like information for a name, including the address that owns it,
+   * the block at which it expires, and the zone file anchored to it (if available).
+   * @param {String} fullyQualifiedName the name to query.  Can be on-chain of off-chain.
+   * @return {Promise} a promise that resolves to the WHOIS-like information 
+   */
   getNameInfo(fullyQualifiedName: string) {
     return fetch(`${this.blockstackAPIUrl}/v1/names/${fullyQualifiedName}`)
       .then((resp) => {
@@ -182,6 +324,11 @@ export class BlockstackNetwork {
       })
   }
 
+  /**
+   * Get the pricing parameters and creation history of a namespace.
+   * @param {String} namespaceID the namespace to query
+   * @return {Promise} a promise that resolves to the namespace information.
+   */
   getNamespaceInfo(namespaceID: string) {
     return fetch(`${this.blockstackAPIUrl}/v1/namespaces/${namespaceID}`)
       .then((resp) => {
@@ -208,18 +355,39 @@ export class BlockstackNetwork {
       })
   }
 
+  /**
+   * Get a zone file, given its hash.  Throws an exception if the zone file
+   * obtained does not match the hash.
+   * @param {String} zonefileHash the ripemd160(sha256) hash of the zone file
+   * @return {Promise} a promise that resolves to the zone file's text
+   */
   getZonefile(zonefileHash: string) {
     return fetch(`${this.blockstackAPIUrl}/v1/zonefiles/${zonefileHash}`)
       .then((resp) => {
         if (resp.status === 200) {
           return resp.text()
-            .then(body => body)
+            .then((body) => {
+              const sha256 = bitcoinjs.crypto.sha256(body)
+              const h = (new RIPEMD160()).update(sha256).digest()
+              if (h !== zonefileHash) {
+                throw new Error(`Zone file contents hash to ${h}, not ${zonefileHash}`)
+              }
+              return body
+            })
         } else {
           throw new Error(`Bad response status: ${resp.status}`)
         }
       })
   }
 
+  /**
+   * Get the status of an account for a particular token holding.  This includes its total number of
+   * expenditures and credits, lockup times, last txid, and so on.
+   * @param {String} address the account
+   * @param {String} tokenType the token type to query
+   * @return {Promise} a promise that resolves to an object representing the state of the account
+   *   for this token
+   */
   getAccountStatus(address: string, tokenType: string) {
     return fetch(`${this.blockstackAPIUrl}/v1/accounts/${address}/${tokenType}/status`)
       .then((resp) => {
@@ -231,16 +399,143 @@ export class BlockstackNetwork {
           return resp.json()
         }
       })
-      .then((accountStatus) => { 
+      .then(accountStatus =>
         // coerce all addresses, and convert credit/debit to biginteger
         Object.assign({}, accountStatus, {
           address: this.coerceAddress(accountStatus.address),
           debit_value: bigi.fromByteArrayUnsigned(String(accountStatus.debit_value)),
           credit_value: bigi.fromByteArrayUnsigned(String(accountStatus.credit_value))
+        }))
+  }
+  
+  
+  /**
+   * Get a page of an account's transaction history.
+   * @param {String} address the account's address
+   * @param {number} page the page number.  Page 0 is the most recent transactions
+   * @return {Promise} a promise that resolves to an Array of Objects, where each Object encodes
+   *   states of the account at various block heights (e.g. prior balances, txids, etc)
+   */
+  getAccountHistoryPage(address: string,
+                        page: number) : Promise<*> {
+    const url = `${this.blockstackAPIUrl}/v1/accounts/${address}/history?page=${page}`
+    return fetch(url)
+      .then((resp) => {
+        if (resp.status === 404) {
+          throw new Error('Account not found')
+        } else if (resp.status !== 200) {
+          throw new Error(`Bad response status: ${resp.status}`)
+        } else {
+          return resp.json()
+        }
+      })
+      .then((historyList) => {
+        if (historyList.error) {
+          throw new Error(`Unable to get account history page: ${historyList.error}`)
+        }
+        // coerse all addresses and convert to bigint
+        return historyList.map((histEntry) => {
+          histEntry.address = this.coerceAddress(histEntry.address)
+          histEntry.debit_value = bigi.fromByteArrayUnsigned(String(histEntry.debit_value))
+          histEntry.credit_value = bigi.fromByteArrayUnsigned(String(histEntry.credit_value))
+          return histEntry
         })
       })
   }
-        
+
+  /**
+   * Get the state(s) of an account at a particular block height.  This includes the state of the
+   * account beginning with this block's transactions, as well as all of the states the account
+   * passed through when this block was processed (if any).
+   * @param {String} address the account's address
+   * @param {Integer} blockHeight the block to query
+   * @return {Promise} a promise that resolves to an Array of Objects, where each Object encodes
+   *   states of the account at this block.
+   */
+  getAccountAt(address: string, blockHeight: number) : Promise<*> {
+    const url = `${this.blockstackAPIUrl}/v1/accounts/${address}/history/${blockHeight}`
+    return fetch(url)
+      .then((resp) => {
+        if (resp.status === 404) {
+          throw new Error('Account not found')
+        } else if (resp.status !== 200) {
+          throw new Error(`Bad response status: ${resp.status}`)
+        } else {
+          return resp.json()
+        }
+      })
+      .then((historyList) => {
+        if (historyList.error) {
+          throw new Error(`Unable to get historic account state: ${historyList.error}`)
+        }
+        // coerce all addresses 
+        return historyList.map((histEntry) => {
+          histEntry.address = this.coerceAddress(histEntry.address)
+          histEntry.debit_value = bigi.fromByteArrayUnsigned(String(histEntry.debit_value))
+          histEntry.credit_value = bigi.fromByteArrayUnsigned(String(histEntry.credit_value))
+          return histEntry
+        })
+      })
+  }
+
+  /**
+   * Get the set of token types that this account owns
+   * @param {String} address the account's address
+   * @return {Promise} a promise that resolves to an Array of Strings, where each item encodes the 
+   *   type of token this account holds (excluding the underlying blockchain's tokens)
+   */
+  getAccountTokens(address: string) : Promise<*> {
+    return fetch(`${this.blockstackAPIUrl}/v1/accounts/${address}/tokens`)
+      .then((resp) => {
+        if (resp.status === 404) {
+          throw new Error('Account not found')
+        } else if (resp.status !== 200) {
+          throw new Error(`Bad response status: ${resp.status}`)
+        } else {
+          return resp.json()
+        }
+      })
+      .then((tokenList) => {
+        if (tokenList.error) {
+          throw new Error(`Unable to get token list: ${tokenList.error}`)
+        }
+        return tokenList
+      })
+  }
+
+  /**
+   * Get the number of tokens owned by an account.  If the account does not exist or has no
+   * tokens of this type, then 0 will be returned.
+   * @param {String} address the account's address
+   * @param {String} tokenType the type of token to query.
+   * @return {Promise} a promise that resolves to a BigInteger that encodes the number of tokens 
+   *   held by this account.
+   */
+  getAccountBalance(address: string, tokenType: string) : Promise<*> {
+    return fetch(`${this.blockstackAPIUrl}/v1/accounts/${address}/${tokenType}/balance`)
+      .then((resp) => {
+        if (resp.status === 404) {
+          // talking to an older blockstack core node without the accounts API
+          return Promise.resolve().then(() => bigi.fromByteArrayUnsigned('0'))
+        } else if (resp.status !== 200) {
+          throw new Error(`Bad response status: ${resp.status}`)
+        } else {
+          return resp.json()
+        }
+      })
+      .then((tokenBalance) => {
+        if (tokenBalance.error) {
+          throw new Error(`Unable to get account balance: ${tokenBalance.error}`)
+        }
+        let balance = '0'
+        if (tokenBalance && tokenBalance.balance) {
+          balance = tokenBalance.balance
+        }
+        return bigi.fromByteArrayUnsigned(balance)
+      })
+  }
+
+
   /**
    * Performs a POST request to the given URL
    * @param  {String} endpoint  the name of
