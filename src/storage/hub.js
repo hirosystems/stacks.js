d8fccf76bd2c8315baa9eaae1f8f70a0fd3b62de
Jude Nelson <judecn@gmail.com>
2018-10-12 12:23:48 -0400

    delint

--- src/storage/hub.js
+++ src/storage/hub.js
@@ -65,9 +65,7 @@ function makeLegacyAuthToken(challengeText: string, signerKeyHex: string): strin
   }
 }
 
-function makeV1GaiaAuthToken(hubInfo: Object,
-                             signerKeyHex: string,
-                             hubUrl: string): string { 
+function makeV1GaiaAuthToken(hubInfo: Object, signerKeyHex: string, hubUrl: string): string { 
   const challengeText = hubInfo.challenge_text
   const handlesV1Auth = (hubInfo.latest_auth_version
                          && parseInt(hubInfo.latest_auth_version.slice(1), 10) >= 1)
@@ -84,7 +82,6 @@ function makeV1GaiaAuthToken(hubInfo: Object,
     iss,
     salt
   }
-  
   const token = new TokenSigner('ES256K', signerKeyHex).sign(payload)
   return `v1:${token}`
 }
