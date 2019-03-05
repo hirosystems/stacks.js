df8960b641b5ddea484cab1ccb3eab7aca9ea53a
Matthew Little <zone117x@gmail.com>
2019-03-05 17:10:20 -0500

    Main src compiling

---
+++ src/@types/bigi/index.d.ts
@@ -0,0 +1,6 @@
+import bigi from 'bigi'
+
+// TODO: get this merged into @types/bigi package
+declare module 'bigi' {
+    // export function fromByteArrayUnsigned(string: string): bigi;
+}
---
+++ src/profiles/profile.ts
@@ -0,0 +1,50 @@
+import inspector from 'schema-inspector'
+
+import { signProfileToken, extractProfile } from './profileTokens'
+import { validateProofs } from './profileProofs'
+import { makeProfileZoneFile } from './profileZoneFiles'
+
+const schemaDefinition: {[key: string]: any} = {
+  type: 'object',
+  properties: {
+    '@context': { type: 'string', optional: true },
+    '@type': { type: 'string' }
+  }
+}
+
+export class Profile {
+
+  _profile: {[key: string]: any}
+
+  constructor(profile = {}) {
+    this._profile = Object.assign({}, {
+      '@context': 'http://schema.org/'
+    }, profile)
+  }
+
+  toJSON() {
+    return Object.assign({}, this._profile)
+  }
+
+  toToken(privateKey) {
+    return signProfileToken(this.toJSON(), privateKey)
+  }
+
+  static validateSchema(profile, strict = false) {
+    schemaDefinition.strict = strict
+    return inspector.validate(schemaDefinition, profile)
+  }
+
+  static fromToken(token, publicKeyOrAddress = null) {
+    const profile = extractProfile(token, publicKeyOrAddress)
+    return new Profile(profile)
+  }
+
+  static makeZoneFile(domainName, tokenFileURL) {
+    return makeProfileZoneFile(domainName, tokenFileURL)
+  }
+
+  static validateProofs(domainName) {
+    return validateProofs(new Profile().toJSON(), domainName)
+  }
+}
