15c05f5bdebebae09cf5f787d395435595833eda
Matthew Little <zone117x@gmail.com>
2019-03-19 18:31:36 -0400

    Rename main module for default exports

--- src/public.ts
+++ src/public.ts
@@ -64,4 +64,4 @@ export { config } from './config'
 
 export { encryptMnemonic, decryptMnemonic } from './encryption'
 
-export { UserSession }  from './auth/userSession'
+export { UserSession } from './auth/userSession'
