cb074991864b2424c0f7f5a2bedbda05c34b3b96
Matthew Little <zone117x@gmail.com>
2019-05-03 15:19:39 -0400

    Simplifiy param null check

--- src/fetchUtil.ts
+++ src/fetchUtil.ts
@@ -1,10 +1,7 @@
 
 /** @ignore */
 export function fetchPrivate(input: RequestInfo, init?: RequestInit): Promise<Response> {
-  if (!init) {
-    init = { referrerPolicy: 'no-referrer' }
-  } else {
-    init.referrerPolicy = 'no-referrer'
-  }
+  init = init || { }
+  init.referrerPolicy = 'no-referrer'
   return fetch(input, init)
 }
