cd0dbfc980310ffe4ebf003eb772789963562bd8
Matthew Little <zone117x@gmail.com>
2019-11-05 11:36:06 -0500

    TODO note in `bn.ts` #2

--- src/bn.ts
+++ src/bn.ts
@@ -7,20 +7,21 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable @typescript-eslint/no-useless-constructor */
 
-// Do not export from the `@types/bn.js` package -- it is improperly typed and requires
-// `syntheticDefaultImports` or `skipLibCheck` to be enabled on consumer packages. 
 
-// @ts-ignore
+// TODO: This is terrible, hacky method to declare the class definition for `bn.js` 
+//       in a way that prevents `@types/bn.js` from causing issues in other libs. The 
+//       `@types/bn.js` package is improperly typed and requires `syntheticDefaultImports` 
+//       or `skipLibCheck` to be enabled on consumer packages. 
+//       This should be replaced with a PR to fix the `@types/bn.js` code.  
+
+
+// @ts-ignore ts(2497): "This module can only be referenced with ECMAScript imports/exports by turning on the 'esModuleInterop' flag and referencing its default export."
 import * as BN_Borked from 'bn.js'
 
 export type Endianness = 'le' | 'be';
 
 const BadTypeUsage = new Error('Class only used as an interface')
 
-// TODO: This is terrible, hacky method to declare the class definition for `bn.js` lib
-//       in a way that prevents `@types/bn.js` from publicly leaking from this this lib
-//       and causing issues in other libs. This should be replaced with a PR to fix 
-//       the `@types/bn.js` code.  
 
 class BNClass {
   constructor(number: number | string | number[] | Uint8Array | Buffer | BNClass, endian?: Endianness);
@@ -30,17 +31,17 @@ class BNClass {
   /**
    * @description  convert to byte Array, and optionally zero pad to length, throwing if already exceeding
    */
-  toArray(_endian?: Endianness, _length?: number): number[] { throw BadTypeUsage }
+  toArray(endian?: Endianness, length?: number): number[] { throw BadTypeUsage }
 
   /**
    * @description  convert to base-string and pad with zeroes
    */
-  toString(_base?: number | 'hex', _length?: number): string { throw BadTypeUsage }
+  toString(base?: number | 'hex', length?: number): string { throw BadTypeUsage }
 
   /**
    * @description compare numbers and return `-1 (a < b)`, `0 (a == b)`, or `1 (a > b)` depending on the comparison result
    */
-  ucmp(_b: BNClass): -1 | 0 | 1 { throw BadTypeUsage }
+  ucmp(b: BNClass): -1 | 0 | 1 { throw BadTypeUsage }
 
   /**
    * @description convert to Javascript Number (limited to 53 bits)
