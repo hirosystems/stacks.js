95cad23e1f4737a4272056f2581f12894fd6f8de
Matthew Little <zone117x@gmail.com>
2019-12-20 10:07:21 -0700

    Clean up changes to `authMessages.ts`

--- src/auth/authMessages.ts
+++ src/auth/authMessages.ts
@@ -118,32 +118,18 @@ export function makeAuthRequest(
 /**
  * Encrypts the private key for decryption by the given
  * public key.
+ * @param  {String} publicKey  [description]
+ * @param  {String} privateKey [description]
+ * @return {String} hex encoded ciphertext
+ * @private
  * @ignore
  */
 export async function encryptPrivateKey(publicKey: string,
-                                        privateKey: string,
-                                        encoding?: 'hex' | 'base64'
-): Promise<string>;
-export async function encryptPrivateKey(publicKey: string,
-                                        privateKey: string,
-                                        encoding: 'buffer'
-): Promise<Buffer>;
-export async function encryptPrivateKey(publicKey: string,
-                                        privateKey: string,
-                                        encoding?: 'hex' | 'base64' | 'buffer'
-): Promise<string | Buffer> {
+                                        privateKey: string
+): Promise<string> {
   const encryptedObj = await encryptECIES(publicKey, Buffer.from(privateKey), true)
   const encryptedJSON = JSON.stringify(encryptedObj)
-  const buffer = Buffer.from(encryptedJSON)
-  if (!encoding || encoding === 'hex') {
-    return buffer.toString('hex')
-  } else if (encoding === 'base64') {
-    return buffer.toString('base64')
-  } else if (encoding === 'buffer') {
-    return buffer
-  } else {
-    throw new Error(`Unexpected encoding ${encoding}`)
-  }
+  return Buffer.from(encryptedJSON).toString('hex')
 }
 
 /**
