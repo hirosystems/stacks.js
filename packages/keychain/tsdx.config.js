5dbc2e9b612333749d473175448c0fd9a2a63e9c
Thomas Osmonson <aulneau@users.noreply.github.com>
2020-10-29 09:21:48 -0500

    fix: try to ignore wordlists for bip39

--- packages/keychain/tsdx.config.js
+++ packages/keychain/tsdx.config.js
@@ -1,5 +1,24 @@
+const emptyFile = 'export default {}';
+const emptyFileName = '\0empty_module';
+
+function ignore(list) {
+  return {
+    resolveId(importee) {
+      return (
+        list.some(function(regex) {
+          return regex.test(importee);
+        }) && emptyFileName
+      );
+    },
+    load(id) {
+      return id === emptyFileName ? emptyFile : null;
+    },
+  };
+}
+
 module.exports = {
   rollup(config, options) {
+    config.plugins.push(ignore([/\.\/wordlists\/(?!english\.json)/]));
     if (options.format === 'esm') {
       config.output = {
         ...config.output,
