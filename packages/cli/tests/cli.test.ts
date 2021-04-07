3dbae3e88a3e58d2cda0d864da80dfb22023d77e
Marcos Casagrande <marcoscvp90@gmail.com>
2021-04-07 16:04:03 +0300

    test(cli): add convert_address tests

--- packages/cli/tests/cli.test.ts
+++ packages/cli/tests/cli.test.ts
@@ -1,4 +1,25 @@
-test('test', () => {
-  expect(true).toBeTruthy()
-})
+import { testables } from '../src/cli';
+import { getNetwork, CLINetworkAdapter, CLI_NETWORK_OPTS } from '../src/network';
+import { CLI_CONFIG_TYPE } from '../src/argparse';
 
+import * as fixtures from './cli.fixture';
+
+const { addressConvert } = testables as any;
+
+const mainnetNetwork = new CLINetworkAdapter(
+  getNetwork({} as CLI_CONFIG_TYPE, false),
+  {} as CLI_NETWORK_OPTS
+);
+
+const testnetNetwork = new CLINetworkAdapter(
+  getNetwork({} as CLI_CONFIG_TYPE, true),
+  {} as CLI_NETWORK_OPTS
+);
+
+describe('convert_address', () => {
+  test.each(fixtures.convertAddress)('%p - testnet: %p', async (input, testnet, expectedResult) => {
+    const network = testnet ? testnetNetwork : mainnetNetwork;
+    const result = await addressConvert(network, [input]);
+    expect(JSON.parse(result)).toEqual(expectedResult);
+  });
+});
