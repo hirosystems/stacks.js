8b444e247dfc95100dd262dba4f4fde2e829d2eb
Marcos Casagrande <marcoscvp90@gmail.com>
2021-04-09 11:34:01 +0300

    test(cli): add tests

--- packages/cli/tests/cli.test.ts
+++ packages/cli/tests/cli.test.ts
@@ -2,7 +2,7 @@ import { testables } from '../src/cli';
 import { getNetwork, CLINetworkAdapter, CLI_NETWORK_OPTS } from '../src/network';
 import { CLI_CONFIG_TYPE } from '../src/argparse';
 
-import * as fixtures from './cli.fixture';
+import * as fixtures from './fixtures/cli.fixture';
 
 const { addressConvert } = testables as any;
 
---
+++ packages/cli/tests/fixtures/keys.fixture.ts
@@ -0,0 +1,55 @@
+import { OwnerKeyInfoType } from '../../src/keys';
+
+export const getOwnerKeyInfo: Array<[string, number, string, OwnerKeyInfoType]> = [
+  [
+    'apart spin rich leader siren foil dish sausage fee pipe ethics bundle', // mnemonic
+    0, // index
+    'v0.10-current', // version
+    {
+      privateKey: 'c0a1c606bf509059be1be27aeb88a60bb530910e16e2e605c7ef9e20a0d6bbb001',
+      idAddress: 'ID-192yxWSvZrss3f4ZY48tUMJJxQvuB6DcXE',
+      version: 'v0.10-current',
+      index: 0,
+    },
+  ],
+  [
+    'apart spin rich leader siren foil dish sausage fee pipe ethics bundle', // mnemonic
+    1, // index
+    'v0.10-current', // version
+    {
+      privateKey: '959ff400904ecc5ea726257e95b91e26e047181720ecb46f92834d7dfd2f694201',
+      idAddress: 'ID-18EaWUxxFc7M36cd2vXszF6qkVz1wdQDao',
+      version: 'v0.10-current',
+      index: 1,
+    },
+  ],
+  [
+    'oppose spider rich leader proud foil dish finger fee pipe ethics bundle', // mnemonic
+    0, // index
+    'v0.10-current', // version
+    {
+      privateKey: '7e96519d1202ad990a4d1d14c9e8891d9c53b52ce1fd4aa1b4549e5fbc8bfcda01',
+      idAddress: 'ID-1FwnbMXxFMxiAFxXV7fKcZn5dB4y6hSkxY',
+      version: 'v0.10-current',
+      index: 0,
+    },
+  ],
+];
+
+export const findIdentityIndex: Array<[string, string, number]> = [
+  [
+    'apart spin rich leader siren foil dish sausage fee pipe ethics bundle', // mnemonic
+    'ID-192yxWSvZrss3f4ZY48tUMJJxQvuB6DcXE',
+    0, // result
+  ],
+  [
+    'apart spin rich leader siren foil dish sausage fee pipe ethics bundle', // mnemonic
+    'ID-18EaWUxxFc7M36cd2vXszF6qkVz1wdQDao',
+    1, // index
+  ],
+  [
+    'oppose spider rich leader proud foil dish finger fee pipe ethics bundle', // mnemonic
+    'ID-1FwnbMXxFMxiAFxXV7fKcZn5dB4y6hSkxY',
+    0, // index
+  ],
+];
