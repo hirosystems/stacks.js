eb30f84875066953bb0759ba6d9e34fadde4d762
Marcos Casagrande <marcoscvp90@gmail.com>
2021-04-09 11:34:01 +0300

    test(cli): add tests

--- packages/cli/tests/keys.test.ts
+++ packages/cli/tests/keys.test.ts
@@ -1,16 +1,18 @@
-import { getStacksWalletKeyInfo } from '../src/keys';
+import { getStacksWalletKeyInfo, getOwnerKeyInfo, findIdentityIndex } from '../src/keys';
 import { getNetwork, CLINetworkAdapter, CLI_NETWORK_OPTS } from '../src/network';
-import { loadConfig, DEFAULT_CONFIG_PATH } from '../src/argparse';
+import { CLI_CONFIG_TYPE } from '../src/argparse';
+
+import * as fixtures from './fixtures/keys.fixture';
 
 // setup
-const networkType = 'mainnet';
-const configData = loadConfig(DEFAULT_CONFIG_PATH, networkType);
-const wrappedNetwork = getNetwork(configData, false);
-const blockstackNetwork = new CLINetworkAdapter(wrappedNetwork, {} as CLI_NETWORK_OPTS);
+const mainnetNetwork = new CLINetworkAdapter(
+  getNetwork({} as CLI_CONFIG_TYPE, false),
+  {} as CLI_NETWORK_OPTS
+);
 
 test('getStacksWalletKeyInfo', async () => {
   const mnemonic = 'apart spin rich leader siren foil dish sausage fee pipe ethics bundle';
-  const info = await getStacksWalletKeyInfo(blockstackNetwork, mnemonic);
+  const info = await getStacksWalletKeyInfo(mainnetNetwork, mnemonic);
 
   expect(info).toEqual({
     privateKey: '25899fab1b9b95cc2d1692529f00fb788e85664df3d14db1a660f33c5f96d8ab01',
@@ -18,5 +20,19 @@ test('getStacksWalletKeyInfo', async () => {
     btcAddress: '1Nwxfx7VoYAg2mEN35dTRw4H7gte8ajFki',
     wif: 'KxUgLbeVeFZEUUQpc3ncYn5KFB3WH5MVRv3SJ2g5yPwkrXs3QRaP',
     index: 0,
+  });
+});
+
+describe('getOwnerKeyInfo', () => {
+  test.each(fixtures.getOwnerKeyInfo)('%#', async (mnemonic, index, version, result) => {
+    const info = await getOwnerKeyInfo(mainnetNetwork, mnemonic, index, version);
+    expect(info).toEqual(result);
+  });
+});
+
+describe('findIdentityIndex', () => {
+  test.each(fixtures.findIdentityIndex)('%#', async (mnemonic, idAddress, result) => {
+    const info = await findIdentityIndex(mainnetNetwork, mnemonic, idAddress);
+    expect(info).toEqual(result);
   });
 });
