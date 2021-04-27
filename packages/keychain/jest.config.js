d55a756f9d987fe25daa68ca9234e8f6c07e844c
Matthew Little <zone117x@gmail.com>
2021-04-27 18:24:00 +0200

    fix: ts-jest path mapping

--- packages/keychain/jest.config.js
+++ packages/keychain/jest.config.js
@@ -3,15 +3,16 @@ module.exports = {
   testEnvironment: 'node',
   coverageDirectory: './coverage/',
   collectCoverage: true,
+  moduleNameMapper: require('jest-module-name-mapper').default(),
   globals: {
     'ts-jest': {
+      tsconfig: './tsconfig.json',
       diagnostics: {
         ignoreCodes: ['TS151001'],
       },
     },
   },
   moduleFileExtensions: ['js', 'ts', 'd.ts'],
-  setupFiles: ['./tests/global-setup.ts'],
-  setupFilesAfterEnv: ['./tests/setup.ts'],
+  setupFilesAfterEnv: ['./tests/setup.js'],
   testTimeout: 10000,
 };
