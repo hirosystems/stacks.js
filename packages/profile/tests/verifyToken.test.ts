9128a4e3432f069d1d8328e95688b891792fa6e5
pradel <pradel.leo@gmail.com>
2021-06-04 09:19:26 +0200

    fix: make `lookupProfile` work with newest stacks API

--- packages/profile/tests/verifyToken.test.ts
+++ packages/profile/tests/verifyToken.test.ts
@@ -8,16 +8,12 @@ const tokenFile = sampleTokenFiles.ryan_apr20.body
 const token = tokenFile[0].token
 
 const publicKey = '02413d7c51118104cfe1b41e540b6c2acaaf91f1e2e22316df7448fb6070d582ec'
-const compressedAddress = '1BTku19roxQs2d54kbYKVTv21oBCuHEApF'
-const uncompressedAddress = '12wes6TQpDF2j8zqvAbXV9KNCGQVF2y7G5'
+const address = 'SPAMWCETADJ9R8D0BXVJKKN3NDMNZ517JSGAG6YX'
 
 test('verifyToken', () => {
   const decodedToken1 = verifyProfileToken(token, publicKey)
   expect(decodedToken1).toBeTruthy()
 
-  const decodedToken2 = verifyProfileToken(token, compressedAddress)
+  const decodedToken2 = verifyProfileToken(token, address)
   expect(decodedToken2).toBeTruthy()
-
-  const decodedToken3 = verifyProfileToken(token, uncompressedAddress)
-  expect(decodedToken3).toBeTruthy()
 })
