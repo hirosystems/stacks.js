1ff5b03c28c664953260105d333cebfa2fd64e5f
Friedger Müffke <mail@friedger.de>
2021-06-19 09:09:10 +0200

    fix: handle empty list in getCVTypeString (#1033)

--- packages/transactions/tests/clarity.test.ts
+++ packages/transactions/tests/clarity.test.ts
@@ -32,7 +32,7 @@ import {
   StandardPrincipalCV,
 } from '../src/clarity';
 import { BufferReader } from '../src/bufferReader';
-import { cvToString, cvToJSON } from '../src/clarity/clarityValue';
+import { cvToString, cvToJSON, getCVTypeString } from '../src/clarity/clarityValue';
 
 const ADDRESS = 'SP2JXKMSH007NPYAQHKJPQMAQYAD90NQGTVJVQ02B';
 
@@ -545,4 +545,48 @@ describe('Clarity Types', () => {
       );
     });
   });
+
+  describe('Clarity Types to String', () => {
+    test('Complex Tuple', () => {
+      const tuple = tupleCV({
+        a: intCV(-1),
+        b: uintCV(1),
+        c: bufferCV(Buffer.from('test')),
+        d: trueCV(),
+        e: someCV(trueCV()),
+        f: noneCV(),
+        g: standardPrincipalCV(ADDRESS),
+        h: contractPrincipalCV(ADDRESS, 'test'),
+        i: responseOkCV(trueCV()),
+        j: responseErrorCV(falseCV()),
+        k: listCV([trueCV(), falseCV()]),
+        l: tupleCV({
+          a: trueCV(),
+          b: falseCV(),
+        }),
+        m: stringAsciiCV('hello world'),
+        n: stringUtf8CV('hello \u{1234}'),
+        o: listCV([])
+      });
+      const typeString = getCVTypeString(tuple)
+      expect(typeString).toEqual(oneLineTrim`
+      (tuple 
+        (a int) 
+        (b uint) 
+        (c (buff 4)) 
+        (d bool) 
+        (e (optional bool)) 
+        (f (optional none)) 
+        (g principal) 
+        (h principal) 
+        (i (response bool UnknownType)) 
+        (j (response UnknownType bool)) 
+        (k (list 2 bool)) 
+        (l (tuple (a bool) (b bool))) 
+        (m (string-ascii 11)) 
+        (n (string-utf8 9)) 
+        (o (list 0 UnknownType)))
+      `)
+    })
+  })
 });
