1670775a493f6c50c93bfe4626872a4e292e5e48
Asim Mehmood <asim@tintash.com>
2021-06-24 17:02:03 +0500

    BREAKING CHANGE: make coreApiUrl readonly for stacks network and initialise in constrctor

--- packages/network/tests/network.test.ts
+++ packages/network/tests/network.test.ts
@@ -1,4 +1,23 @@
-test('test', () => {
-  expect(true).toBeTruthy()
-})
+import {
+  HIRO_MAINNET_DEFAULT,
+  HIRO_MOCKNET_DEFAULT,
+  HIRO_TESTNET_DEFAULT,
+  StacksMainnet,
+  StacksMocknet,
+  StacksTestnet,
+} from '@stacks/network';
 
+test('network test-- coreApiUrl', () => {
+  const mainnet = new StacksMainnet();
+  expect(mainnet.coreApiUrl).toBe(HIRO_MAINNET_DEFAULT);
+
+  const testnet = new StacksTestnet();
+  expect(testnet.coreApiUrl).toBe(HIRO_TESTNET_DEFAULT);
+
+  const mocknet = new StacksMocknet();
+  expect(mocknet.coreApiUrl).toBe(HIRO_MOCKNET_DEFAULT);
+
+  const customURL = 'customeURL';
+  const customNET = new StacksMainnet({ url: customURL });
+  expect(customNET.coreApiUrl).toBe(customURL);
+});
