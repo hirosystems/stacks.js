9faa89e7a554953a713a420b55e4e3f457715fb9
Gregory Coppola <60008382+gregorycoppola@users.noreply.github.com>
2021-07-16 16:21:52 -0500

    simple profile script

--- packages/cli/run_profile.sh
+++ packages/cli/run_profile.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+NUM_ROUNDS=$1
+
+if [ -z ${NUM_ROUNDS} ];
+then
+	echo "NUM_ROUNDS is not set";
+	exit 1
+else
+	echo "NUM_ROUNDS: ${NUM_ROUNDS}";
+fi
+
 npm run build
 
 if [ $? -eq 0 ]
@@ -8,4 +19,9 @@ else
 	exit 1
 fi
 
-node dist/index.js -I http://localhost:20443 call_read_only_contract_func SP000000000000000000002Q6VF78 pox get-pox-info SPEET2XN4XHZNX8R374HKCNMEJDMEA0B8KBXRF7Q
+for ((i = 0 ; i < ${NUM_ROUNDS} ; i++)); do
+	echo Simulation: $i
+
+	node dist/index.js -I http://localhost:20443 call_read_only_contract_func SP000000000000000000002Q6VF78 pox get-pox-info SPEET2XN4XHZNX8R374HKCNMEJDMEA0B8KBXRF7Q
+
+done
