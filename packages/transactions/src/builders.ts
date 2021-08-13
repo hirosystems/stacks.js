9e0f494c88f1a3ac123eabbe41e36135b2ec3a54
Reed Rosenbluth <331327+reedrosenbluth@users.noreply.github.com>
2021-08-12 22:01:00 -0400

    style: prettier

--- packages/transactions/src/builders.ts
+++ packages/transactions/src/builders.ts
@@ -1199,8 +1199,14 @@ export async function callReadOnlyFunction(
 
   const options = Object.assign(defaultOptions, readOnlyFunctionOptions);
 
-  const { contractName, contractAddress, functionName, functionArgs, network, senderAddress } =
-    options;
+  const {
+    contractName,
+    contractAddress,
+    functionName,
+    functionArgs,
+    network,
+    senderAddress,
+  } = options;
 
   const url = network.getReadOnlyFunctionCallApiUrl(contractAddress, contractName, functionName);
 
