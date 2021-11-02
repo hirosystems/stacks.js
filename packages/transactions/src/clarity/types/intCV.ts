f0334cf8581bc5aced724e8f151a826018e0f3f8
AhsanJavaid <ahsan.javaid@tintash.com>
2021-11-02 15:46:35 +0500

    fix: add custom bigint pow to prevent transpilers incorrect conversion

--- packages/transactions/src/clarity/types/intCV.ts
+++ packages/transactions/src/clarity/types/intCV.ts
@@ -1,10 +1,11 @@
 import { IntegerType, intToBigInt } from '@stacks/common';
 import { ClarityType } from '../constants';
 
-const MAX_U128 = BigInt(2) ** BigInt(128) - BigInt(1);
+const MAX_U128 = BigInt('0xffffffffffffffffffffffffffffffff'); // (2 ** 128 - 1)
 const MIN_U128 = BigInt(0);
-const MAX_I128 = BigInt(2) ** BigInt(127) - BigInt(1);
-const MIN_I128 = BigInt(-2) ** BigInt(127);
+const MAX_I128 = BigInt('0x7fffffffffffffffffffffffffffffff'); // (2 ** 127 - 1)
+// no signed (negative) hex support in bigint constructor
+const MIN_I128 = BigInt('-170141183460469231731687303715884105728'); // (-2 ** 127)
 
 interface IntCV {
   readonly type: ClarityType.Int;
