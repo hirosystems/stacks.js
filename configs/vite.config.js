1b6175e78658066575da1400e0e81c56c6f23adf
janniks <janniks@users.noreply.github.com>
2022-02-22 03:25:50 +0100

    give up on vite :(

--- configs/vite.config.js
+++ configs/vite.config.js
@@ -1,79 +1,88 @@
 import { defineConfig } from 'vite';
-import typescript from '@rollup/plugin-typescript';
-import autoExternal from 'rollup-plugin-auto-external';
-import polyfill from 'rollup-plugin-node-polyfills';
-import { babel, getBabelOutputPlugin } from '@rollup/plugin-babel';
 
-import externals from 'rollup-plugin-node-externals';
-import inject from '@rollup/plugin-inject';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
 import alias from '@rollup/plugin-alias';
-
-import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
-
-import path from 'path';
-
-const customResolver = resolve({
-  extensions: ['.js'],
-  preferBuiltins: false,
-});
+import commonjs from '@rollup/plugin-commonjs';
+import inject from '@rollup/plugin-inject';
+import json from '@rollup/plugin-json';
+import resolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import typescript from '@rollup/plugin-typescript';
+import globals from 'rollup-plugin-node-globals';
 
 export default defineConfig({
   build: {
     sourcemap: true,
     lib: {
       entry: 'src/index.ts',
-      fileName: () => 'vite.js',
+      fileName: () => 'polyfill/index.js',
       formats: ['es'],
     },
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: [/\@stacks\/.*/],
+      external: [/\@stacks\/.*\/dist\/polyfill/],
       output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        // globals: {
-        //   vue: 'Vue',
-        // },
+        // file: 'dist/polyfill/index.js',
+        format: 'esm',
+        intro: 'window.global = window',
       },
     },
+    commonjsOptions: {
+      ignoreTryCatch: false,
+      transformMixedEsModules: true,
+    },
   },
+  resolve: { mainFields: ['browser', 'module', 'main', 'jsnext:main'] },
   // esbuild: false,
   plugins: [
     alias({
-      // customResolver,
-      entries: [{ find: /inherits.*/, replacement: require.resolve('inherits/inherits_browser') }],
-    }),
-    resolve({ preferBuiltins: true }), // before commonjs
+      entries: [
+        // polyfills
+        { find: 'stream', replacement: 'vite-compatible-readable-stream' },
+        { find: 'crypto', replacement: 'crypto-browserify' },
+        { find: 'readable-stream', replacement: 'vite-compatible-readable-stream' },
+        // { find: 'brorand', replacement: require.resolve('./polyfills/brorand.js') },
+        { find: 'randombytes', replacement: 'randombytes/browser' },
 
-    polyfill({ crypto: true }),
-    commonjs({}), // before babel
+        // replace @stacks with their polyfill versions
+        { find: '@stacks/auth', replacement: '@stacks/auth/dist/polyfill' },
+        { find: '@stacks/bns', replacement: '@stacks/bns/dist/polyfill' },
+        { find: '@stacks/common', replacement: '@stacks/common/dist/polyfill' },
+        { find: '@stacks/encryption', replacement: '@stacks/encryption/dist/polyfill' },
+        { find: '@stacks/keychain', replacement: '@stacks/keychain/dist/polyfill' },
+        { find: '@stacks/network', replacement: '@stacks/network/dist/polyfill' },
+        { find: '@stacks/profile', replacement: '@stacks/profile/dist/polyfill' },
+        { find: '@stacks/stacking', replacement: '@stacks/stacking/dist/polyfill' },
+        { find: '@stacks/storage', replacement: '@stacks/storage/dist/polyfill' },
+        { find: '@stacks/transactions', replacement: '@stacks/transactions/dist/polyfill' },
+        { find: '@stacks/wallet-sdk', replacement: '@stacks/wallet-sdk/dist/polyfill' },
+      ],
+    }),
+    resolve({
+      mainFields: ['browser', 'module', 'main', 'jsnext:main'],
+      preferBuiltins: false,
+    }),
+    // commonjs({
+    //   ignoreTryCatch: false,
+    //   transformMixedEsModules: true,
+    // }),
+    replace({
+      values: {
+        'process.browser': true,
+        'process.env.NODE_ENV': false,
+        'process.env.NODE_DEBUG': false,
+      },
+    }),
+    globals(),
+    inject({
+      Buffer: ['buffer', 'Buffer'],
+    }),
     typescript({
       tsconfig: './tsconfig.build.json',
+      // todo: disable delcation dist building
     }),
-    // inject({
-    //   // Buffer: [require.resolve('./shim.js'), 'Buffer'],
-    //   Buffer: ['buffer', 'Buffer'],
-    //   // include: /.*\.js$/,
-    // }),
-    // babel({ babelHelpers: 'runtime' }),
-    // getBabelOutputPlugin({
-    //   presets: ['@babel/preset-env'],
-    //   plugins: [['@babel/plugin-transform-runtime', { useESModules: true }]],
-    // }),
+    json({
+      compact: true,
+      // todo: ignore non-english wordlists (currently causes commonjs to ignore needed deps)
+      // proposed regex: include: /^(?!.*wordlist(?!.*english)).*/,
+    }),
   ],
-  // resolve: {
-  //   alias: {
-  //     // process: 'process/browser',
-  //     // stream: 'stream-browserify',
-  //     // zlib: 'browserify-zlib',
-  //     // util: 'util',
-  //     inherits: 'inherits/inherits_browser',
-  //   },
-  // },
-  optimizeDeps: {
-    exclude: ['sha.js'],
-  },
 });
