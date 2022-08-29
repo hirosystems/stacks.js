0062a453fec80de93d36e0d8c5e3f37a7522c300
Matthew Little <zone117x@gmail.com>
2022-08-29 14:20:04 +0200

    feat: support `versioned-smart-contract` tx types introduced in Stacks 2.1 (#1341)

--- packages/transactions/src/constants.ts
+++ packages/transactions/src/constants.ts
@@ -41,9 +41,15 @@ export function whenMessageType(messageType: StacksMessageType) {
 enum PayloadType {
   TokenTransfer = 0x00,
   SmartContract = 0x01,
+  VersionedSmartContract = 0x06,
   ContractCall = 0x02,
   PoisonMicroblock = 0x03,
   Coinbase = 0x04,
+}
+
+enum ClarityVersion {
+  Clarity1 = 1,
+  Clarity2 = 2,
 }
 
 /**
@@ -178,6 +184,7 @@ export {
   ChainID,
   StacksMessageType,
   PayloadType,
+  ClarityVersion,
   AnchorMode,
   TransactionVersion,
   PostConditionMode,
