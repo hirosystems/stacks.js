56fef680d7ae4e85209e8866cb7157b7ed3ee0a3
janniks <janniks@users.noreply.github.com>
2023-04-05 15:33:50 -0600

    docs: update comments

--- packages/encryption/src/cryptoRandom.ts
+++ packages/encryption/src/cryptoRandom.ts
@@ -1,17 +1,17 @@
 import { utils } from '@noble/secp256k1';
 
 /**
- * Reexports @noble/secp256k1's randombytes
+ * Reexports @noble/secp256k1's `randomBytes`.
  * Generates bytes with random bytes of given length
- * @param bytesLength an optional bytes length, default `32` bytes
+ * @param bytesLength an optional bytes length, default `32`
  * @return {Uint8Array} random bytes
  */
 export const randomBytes = (bytesLength: number = 32): Uint8Array => utils.randomBytes(bytesLength);
 
 /**
- * Reexports @noble/secp256k1's randombytes
+ * Reexports @noble/secp256k1's `randomPrivateKey`.
  * Generates a random scalar between 0 and the group prime for secp256k1
- * @returns {Uint8Array}
+ * @returns {Uint8Array} random private key
  */
 export const randomPrivateKey = (): Uint8Array => utils.randomPrivateKey();
 
