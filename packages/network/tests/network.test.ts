9f6c5d296003d81eaf23d021af4ecc5bcd21dfe1
janniks <janniks@users.noreply.github.com>
2023-04-06 13:23:36 -0600

    fix: add StacksDevnet constructor, closes #1470

--- packages/network/tests/network.test.ts
+++ packages/network/tests/network.test.ts
@@ -4,25 +4,33 @@ import {
   HIRO_TESTNET_DEFAULT,
   StacksMainnet,
   StacksMocknet,
+  StacksNetwork,
   StacksTestnet,
 } from '../src/network';
 
 describe('Setting coreApiUrl', () => {
-  test('it sets mainnet default url', () => {
+  it('sets mainnet default url', () => {
     const mainnet = new StacksMainnet();
     expect(mainnet.coreApiUrl).toEqual(HIRO_MAINNET_DEFAULT);
   });
-  test('it sets testnet url', () => {
+  it('sets testnet url', () => {
     const testnet = new StacksTestnet();
     expect(testnet.coreApiUrl).toEqual(HIRO_TESTNET_DEFAULT);
   });
-  test('it sets mocknet url', () => {
+  it('sets mocknet url', () => {
     const mocknet = new StacksMocknet();
     expect(mocknet.coreApiUrl).toEqual(HIRO_MOCKNET_DEFAULT);
   });
-  test('it sets custom url', () => {
+  it('sets custom url', () => {
     const customURL = 'https://customurl.com';
     const customNET = new StacksMainnet({ url: customURL });
     expect(customNET.coreApiUrl).toEqual(customURL);
   });
+});
+
+it('uses the correct constructor for stacks network from name strings', () => {
+  expect(StacksNetwork.fromName('mainnet').constructor.toString()).toContain('StacksMainnet');
+  expect(StacksNetwork.fromName('testnet').constructor.toString()).toContain('StacksTestnet');
+  expect(StacksNetwork.fromName('devnet').constructor.toString()).toContain('StacksMocknet'); // devnet is an alias for mocknet
+  expect(StacksNetwork.fromName('mocknet').constructor.toString()).toContain('StacksMocknet');
 });
