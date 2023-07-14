a6fb26ef46af6c53925272f08ce39e1a85298ff6
janniks <janniks@users.noreply.github.com>
2023-07-14 10:19:31 +0200

    fix: add network id

--- packages/common/src/constants.ts
+++ packages/common/src/constants.ts
@@ -1,8 +1,27 @@
-export enum ChainID {
-  Testnet = 0x80000000,
-  Mainnet = 0x00000001,
+/**
+ * The NetworkID is used to identify the blockchain network in p2p.
+ * It is NOT used in signing or verifying signatures.
+ */
+export enum NetworkID {
+  Mainnet = 0x17000000, // 385875968
+  Testnet = 0xff000000, // 4278190080
 }
 
+/**
+ * The ChainID is used to identify the blockchain chain in transactions.
+ * It is used in the signed transaction serialization.
+ * It should be unique, to prevent replay attacks across different chains.
+ */
+export enum ChainID {
+  Mainnet = 0x00000001,
+  Testnet = 0x80000000,
+}
+
+/**
+ * The TransactionVersion is an additional network identifier used in
+ * transactions.
+ * It is used in the signed transaction serialization.
+ */
 export enum TransactionVersion {
   Mainnet = 0x00,
   Testnet = 0x80,
