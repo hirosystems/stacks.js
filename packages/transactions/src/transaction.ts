1be43cdeb58cba51086bda00ff6ceff19896c25e
janniks <janniks@users.noreply.github.com>
2024-02-05 17:30:02 +0100

    chore: resolve circular dependency

--- packages/transactions/src/transaction.ts
+++ packages/transactions/src/transaction.ts
@@ -19,6 +19,7 @@ import {
   deserializeAuthorization,
   intoInitialSighashAuth,
   isSingleSig,
+  MultiSigSpendingCondition,
   nextSignature,
   serializeAuthorization,
   setFee,
@@ -30,6 +31,7 @@ import {
 } from './authorization';
 import { BytesReader } from './bytesReader';
 import {
+  AddressHashMode,
   AnchorMode,
   anchorModeFrom,
   AnchorModeName,
@@ -37,6 +39,7 @@ import {
   PayloadType,
   PostConditionMode,
   PubKeyEncoding,
+  RECOVERABLE_ECDSA_SIG_LENGTH_BYTES,
   StacksMessageType,
 } from './constants';
 import { SerializationError, SigningError } from './errors';
@@ -305,4 +308,42 @@ export function deriveNetworkFromTx(transaction: StacksTransaction) {
     [TransactionVersion.Mainnet]: STACKS_MAINNET,
     [TransactionVersion.Testnet]: STACKS_TESTNET,
   });
+}
+
+/**
+ * Estimates transaction byte length
+ * Context:
+ * 1) Multi-sig transaction byte length increases by adding signatures
+ *    which causes the incorrect fee estimation because the fee value is set while creating unsigned transaction
+ * 2) Single-sig transaction byte length remain same due to empty message signature which allocates the space for signature
+ * @param {transaction} - StacksTransaction object to be estimated
+ * @return {number} Estimated transaction byte length
+ */
+export function estimateTransactionByteLength(transaction: StacksTransaction): number {
+  const hashMode = transaction.auth.spendingCondition.hashMode;
+  // List of Multi-sig transaction hash modes
+  const multiSigHashModes = [AddressHashMode.SerializeP2SH, AddressHashMode.SerializeP2WSH];
+
+  // Check if its a Multi-sig transaction
+  if (multiSigHashModes.includes(hashMode)) {
+    const multiSigSpendingCondition: MultiSigSpendingCondition = transaction.auth
+      .spendingCondition as MultiSigSpendingCondition;
+
+    // Find number of existing signatures if the transaction is signed or partially signed
+    const existingSignatures = multiSigSpendingCondition.fields.filter(
+      field => field.contents.type === StacksMessageType.MessageSignature
+    ).length; // existingSignatures will be 0 if its a unsigned transaction
+
+    // Estimate total signature bytes size required for this multi-sig transaction
+    // Formula: totalSignatureLength = (signaturesRequired - existingSignatures) * (SIG_LEN_BYTES + 1 byte for type of signature)
+    const totalSignatureLength =
+      (multiSigSpendingCondition.signaturesRequired - existingSignatures) *
+      (RECOVERABLE_ECDSA_SIG_LENGTH_BYTES + 1);
+
+    return transaction.serialize().byteLength + totalSignatureLength;
+  } else {
+    // Single-sig transaction
+    // Signature space already allocated by empty message signature
+    return transaction.serialize().byteLength;
+  }
 }
