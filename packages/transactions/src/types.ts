0cd4885a48e82465dff5b2ccc1675f7dc3cbadf7
janniks <janniks@users.noreply.github.com>
2024-02-20 17:44:06 +0100

    test: add more cases to make clear

--- packages/transactions/src/types.ts
+++ packages/transactions/src/types.ts
@@ -274,7 +274,7 @@ export function serializeMemoString(memoString: MemoString): Uint8Array {
 
 export function deserializeMemoString(bytesReader: BytesReader): MemoString {
   let content = bytesToUtf8(bytesReader.readBytes(MEMO_MAX_LENGTH_BYTES));
-  content = content.replace(/\u0000*$/, ''); // remove all trailing unicode empty characters
+  content = content.replace(/\u0000*$/, ''); // remove all trailing unicode null characters
   return { type: StacksMessageType.MemoString, content };
 }
 
