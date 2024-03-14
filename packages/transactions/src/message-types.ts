5ca16380e298c36d472dcadf11787f0003a1c500
janniks <6362150+janniks@users.noreply.github.com>
2024-03-14 15:38:27 +0100

    chore: fine
    Co-authored-by: Micaiah Reid <micaiahreid@gmail.com>

--- packages/transactions/src/message-types.ts
+++ packages/transactions/src/message-types.ts
@@ -1,5 +1,5 @@
 // todo: this file should hold the type definitions for more message types later
-//       needed now to fix a circular dependency issue in structuredDataSignature
+// needed now to fix a circular dependency issue in structuredDataSignature
 import { StacksMessageType } from './constants';
 
 export interface StructuredDataSignature {
