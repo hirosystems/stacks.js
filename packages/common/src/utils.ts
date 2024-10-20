0ca63481729e4b1a7b860115330acbca48974dd1
janniks <janniks@users.noreply.github.com>
2024-10-20 02:26:08 +0200

    refactor: Undo removal

--- packages/common/src/utils.ts
+++ packages/common/src/utils.ts
@@ -50,6 +50,18 @@ export function megabytesToBytes(megabytes: number): number {
 }
 
 /**
+ * Calculate the AES-CBC ciphertext output byte length a given input length.
+ * AES has a fixed block size of 16-bytes regardless key size.
+ * @ignore
+ * @deprecated
+ */
+export function getAesCbcOutputLength(inputByteLength: number) {
+  // AES-CBC block mode rounds up to the next block size.
+  const cipherTextLength = (Math.floor(inputByteLength / 16) + 1) * 16;
+  return cipherTextLength;
+}
+
+/**
  * Calculate the base64 encoded string length for a given input length.
  * This is equivalent to the byte length when the string is ASCII or UTF8-8
  * encoded.
